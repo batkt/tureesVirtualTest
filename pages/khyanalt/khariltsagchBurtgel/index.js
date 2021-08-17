e1102214b29fbfb977ed34f9d983c440dfc5f649
sumkaMA <sumkakh@gmail.com>
2021-08-17 21:33:36 +0800

    shinechlelt

--- pages/khyanalt/khariltsagchBurtgel/index.js
+++ pages/khyanalt/khariltsagchBurtgel/index.js
@@ -11,7 +11,7 @@ import {
   Form,
   Popconfirm,
   Popover,
-  Empty,
+  Tag,
 } from "antd"
 import {
   UserOutlined,
@@ -23,6 +23,7 @@ import {
   MailOutlined,
   SecurityScanOutlined,
   BellOutlined,
+  FileTextOutlined,
 } from "@ant-design/icons"
 import shalgaltKhiikh from "../../../services/shalgaltKhiikh"
 
@@ -31,8 +32,11 @@ import uilchilgee, { aldaaBarigch, url } from "../../../services/uilchilgee"
 import { useAuth } from "../../../services/auth"
 import React, { useState, useRef } from "react"
 import moment from "moment"
-import { useAjiltniiJagsaalt } from "hooks/useAjiltan"
+import { useKhariltsagch } from "hooks/useKhariltsagch"
 import getBase64 from "tools/function/getBase64"
+import deleteMethod from "tools/function/crud/deleteMethod"
+import createMethod from "tools/function/crud/createMethod"
+import updateMethod from "tools/function/crud/updateMethod"
 
 const iconColor = { fontSize: "18px" }
 
@@ -42,85 +46,66 @@ function AjiltanBurtgel({ token }) {
   const empty = useRef()
 
   const { ajiltan, baiguullaga } = useAuth()
-  const { ajilchdiinGaralt, setAjiltniiKhuudaslalt, ajiltniiJagsaaltMutate } =
-    useAjiltniiJagsaalt(token, baiguullaga?._id)
+  const { khariltsagchiinGaralt, setKhuudaslalt, khariltsagchMutate } =
+    useKhariltsagch(token, baiguullaga?._id)
+  const [formNuukh, setFormNuukh] = useState(false)
 
-  const [ajiltanState, setAjiltanState] = useState({
+  const [khariltsagchState, setkhariltsagchState] = useState({
     ner: undefined,
     ovog: undefined,
     register: undefined,
     khayag: undefined,
     utas: undefined,
-    albanTushaal: undefined,
+    email: undefined,
+    turul: undefined,
+    tuluv: undefined,
     baiguullagiinId: ajiltan?.baiguullagiinId,
   })
 
   const { Option } = Select
 
   function onChange(talbar, utga) {
-    setAjiltanState((a) => ({ ...a, [talbar]: utga }))
+    setkhariltsagchState((a) => ({ ...a, [talbar]: utga }))
   }
-  function ajiltanBurtgekh() {
-    if (ajiltanState.nuutsUg && ajiltanState.nuutsUg.length < 2) {
-      message.warning("Нууц үг буруу оруулсан байна.")
-      return
-    }
-
+  function khariltsagchBurtgekh() {
     var form_data = new FormData()
-    ajiltanState.baiguullagiinId = ajiltan?.baiguullagiinId
-    switch (ajiltanState.albanTushaal) {
-      case "Админ":
-        ajiltanState.erkh = "Admin"
-        break
-      case "Зохион байгуулагч":
-        ajiltanState.erkh = "ZokhionBaiguulagch"
-        break
-      case "Санхүү":
-        ajiltanState.erkh = "Sankhuu"
-        break
-      default:
-        break
+    khariltsagchState.baiguullagiinId = ajiltan?.baiguullagiinId
+    if (khariltsagchState.zasakhEsekh === true) {
+      updateMethod("khariltsagch", token, khariltsagchState)
+        .then(({ data }) => {
+          if (data !== undefined) {
+            message.success("Бүртгэл амжилттай засагдлаа")
+            formRef.current.resetFields()
+            khariltsagchMutate((s) => ({ ...s, jagsaalt: s.jagsaalt }), true)
+          }
+        })
+        .catch(aldaaBarigch)
+    } else {
+      createMethod("khariltsagch", token, khariltsagchState)
+        .then(({ data }) => {
+          if (data !== undefined) {
+            message.success("Бүртгэл амжилттай хийгдлээ")
+            formRef.current.resetFields()
+            khariltsagchMutate((s) => ({ ...s, jagsaalt: s.jagsaalt }), true)
+          }
+        })
+        .catch(aldaaBarigch)
     }
-    for (var key in ajiltanState) {
-      form_data.append(key, ajiltanState[key])
-    }
-    uilchilgee(token)
-      .post("/ajiltan", form_data)
-      .then(({ data }) => {
-        if (data !== undefined) {
-          message.success("Бүртгэл амжилттай хийгдлээ")
-          formRef.current.resetFields()
-          ajiltniiJagsaaltMutate((s) => ({ ...s, jagsaalt: s.jagsaalt }), true)
-        }
-      })
-      .catch(aldaaBarigch)
   }
 
   function zasya(data) {
     data.zasakhEsekh = true
-    if (!!data.zurgiinNer) {
-      zurag.current.src = `${url}/ajiltniiZuragAvya/${data.baiguullagiinId}/${data.zurgiinNer}`
-      zurag.current.classList.remove("hidden")
-      empty.current.classList.add("hidden")
-    }
-    data.ajildOrsonOgnoo = moment(data.ajildOrsonOgnoo)
     formRef.current.setFieldsValue({ ...data })
-    setAjiltanState(data)
+    setkhariltsagchState(data)
   }
 
-  function ajiltanUstgay(mur) {
-    if (ajiltan._id === mur._id) {
-      message.warning("Та өөрийгөө устгаж болохгүй!")
-      return
-    }
-    uilchilgee(token)
-      .post("/ajiltanUstgay", { id: mur._id })
-      .then(({ data }) => {
-        if (data === "Amjilttai") {
-          ajiltniiJagsaaltMutate((s) => ({ ...s, jagsaalt: s.jagsaalt }), true)
-          message.success("Устгагдлаа")
-        }
-      })
+  function khariltsagchUstgay(mur) {
+    deleteMethod("khariltsagch", token, mur._id).then(({ data }) => {
+      if (data !== undefined) {
+        khariltsagchMutate((s) => ({ ...s, jagsaalt: s.jagsaalt }), true)
+        message.success("Устгагдлаа")
+      }
+    })
   }
 
   const props = {
@@ -139,11 +124,11 @@ function AjiltanBurtgel({ token }) {
   }
 
   function onFinish() {
-    ajiltanBurtgekh()
+    khariltsagchBurtgekh()
   }
   function checkRegister() {
-    var value1 = ajiltanState.register.substring(0, 2)
-    var value2 = ajiltanState.register.substring(2, 10)
+    var value1 = khariltsagchState.register.substring(0, 2)
+    var value2 = khariltsagchState.register.substring(2, 10)
     var error = 0
     for (var i = 0; i < 2; i++) {
       var c = value1.charCodeAt(i)
@@ -172,25 +157,25 @@ function AjiltanBurtgel({ token }) {
       }
     }
     if (
-      ajiltanState.register.length <= 2 ||
-      ajiltanState.register.length > 10 ||
+      khariltsagchState.register.length <= 2 ||
+      khariltsagchState.register.length > 10 ||
       error > 0
     ) {
-      ajiltanState.register = value1.toUpperCase() + value2
+      khariltsagchState.register = value1.toUpperCase() + value2
     }
-    if (ajiltanState.register.length === 10) {
-      var year = parseInt(ajiltanState.register.substring(2, 4))
-      var month = parseInt(ajiltanState.register.substring(4, 6))
+    if (khariltsagchState.register.length === 10) {
+      var year = parseInt(khariltsagchState.register.substring(2, 4))
+      var month = parseInt(khariltsagchState.register.substring(4, 6))
       month = month - 1
-      var day = parseInt(ajiltanState.register.substring(6, 8))
+      var day = parseInt(khariltsagchState.register.substring(6, 8))
       var nowYear = new Date().getFullYear().toString().substring(2, 4)
       if (month > 32 || (12 < month && month < 21)) {
         message.warning("Регистерийн дугаарын сар буруу байна!")
-        ajiltanState.register = ""
+        khariltsagchState.register = ""
         return
       } else if (year > nowYear && 21 <= month && month <= 32) {
         message.warning("Регистерийн дугаарын жил, сарын хослол буруу байна!")
-        ajiltanState.register = ""
+        khariltsagchState.register = ""
         return
       }
 
@@ -204,6 +189,9 @@ function AjiltanBurtgel({ token }) {
         return
       }
     }
+  }
+  function turulSongokh(value) {
+    setFormNuukh(value)
   }
   return (
     <Admin
@@ -234,6 +222,26 @@ function AjiltanBurtgel({ token }) {
           initialValues={{ remember: true }}
         >
           <Form.Item
+            name="turul"
+            rules={[
+              {
+                required: true,
+                message: "Төрөл сонгоно уу!",
+              },
+            ]}
+          >
+            <Select
+              style={{ width: "100%" }}
+              value={khariltsagchState.turul}
+              placeholder={"Төрөл сонгох"}
+              onChange={turulSongokh}
+            >
+              <Option value="Иргэн">Иргэн</Option>
+              <Option value="ААН">ААН</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            hidden={formNuukh === "ААН" ? true : false}
             name="ovog"
             rules={[
               {
@@ -246,7 +254,7 @@ function AjiltanBurtgel({ token }) {
               type="text"
               allowClear
               placeholder="овог"
-              value={ajiltanState.ovog}
+              value={khariltsagchState.ovog}
               prefix={<UserOutlined style={iconColor} />}
               onChange={(e) => onChange("ovog", e.target.value)}
             ></Input>
@@ -264,7 +272,7 @@ function AjiltanBurtgel({ token }) {
               type="text"
               allowClear
               placeholder="нэр"
-              value={ajiltanState.ner}
+              value={khariltsagchState.ner}
               prefix={<UserOutlined style={iconColor} />}
               onChange={(e) => onChange("ner", e.target.value)}
             ></Input>
@@ -284,10 +292,10 @@ function AjiltanBurtgel({ token }) {
               allowClear
               maxLength={10}
               placeholder="регистр"
-              value={ajiltanState.register}
+              value={khariltsagchState.register}
               onChange={(e) => onChange("register", e.target.value)}
               prefix={<SolutionOutlined style={iconColor} />}
-              onBlur={() => checkRegister()}
+              onBlur={() => (formNuukh === "ААН" ? "" : checkRegister())}
             ></Input>
           </Form.Item>
           <Form.Item
@@ -302,7 +310,7 @@ function AjiltanBurtgel({ token }) {
             <Input
               allowClear
               placeholder="хаяг"
-              value={ajiltanState.khayag}
+              value={khariltsagchState.khayag}
               onChange={(e) => onChange("khayag", e.target.value)}
               prefix={<HomeOutlined style={iconColor} />}
             ></Input>
@@ -320,16 +328,25 @@ function AjiltanBurtgel({ token }) {
             <Input
               allowClear
               placeholder="утас"
-              value={ajiltanState.utas}
+              value={khariltsagchState.utas}
               onChange={(e) => onChange("utas", e.target.value)}
               prefix={<PhoneOutlined style={iconColor} />}
             ></Input>
+          </Form.Item>
+          <Form.Item name="mail">
+            <Input
+              type="email"
+              placeholder="и-мэйл хаяг"
+              value={khariltsagchState.email}
+              onChange={(e) => onChange("mail", e.target.value)}
+              prefix={<MailOutlined style={iconColor} />}
+            />
           </Form.Item>
 
           <Form.Item>
             <Button
               //htmlType="submit"
-              //onClick={ajiltanBurtgekh}
+              onClick={khariltsagchBurtgekh}
               style={{ backgroundColor: "#209669", color: "#ffffff" }}
             >
               хадгалах
@@ -354,17 +371,17 @@ function AjiltanBurtgel({ token }) {
         <Table
           bordered
           tableLayout={
-            ajilchdiinGaralt?.jagsaalt?.length > 0 ? "auto" : "fixed"
+            khariltsagchiinGaralt?.jagsaalt?.length > 0 ? "auto" : "fixed"
           }
           rowKey={(row) => row._id}
-          dataSource={[]}
+          dataSource={khariltsagchiinGaralt?.jagsaalt}
           pagination={{
-            current: ajilchdiinGaralt?.khuudasniiDugaar,
-            pageSize: ajilchdiinGaralt?.khuudasniiKhemjee,
-            total: ajilchdiinGaralt?.niitMur,
+            current: khariltsagchiinGaralt?.khuudasniiDugaar,
+            pageSize: khariltsagchiinGaralt?.khuudasniiKhemjee,
+            total: khariltsagchiinGaralt?.niitMur,
             showSizeChanger: true,
             onChange: (khuudasniiDugaar, khuudasniiKhemjee) =>
-              setAjiltniiKhuudaslalt((kh) => ({
+              setKhuudaslalt((kh) => ({
                 ...kh,
                 khuudasniiDugaar,
                 khuudasniiKhemjee,
@@ -377,32 +394,55 @@ function AjiltanBurtgel({ token }) {
               key: "index",
               className: "text-center",
               render: (text, record, index) =>
-                (ajilchdiinGaralt?.khuudasniiDugaar || 0) *
-                  (ajilchdiinGaralt?.khuudasniiKhemjee || 0) -
-                (ajilchdiinGaralt?.khuudasniiKhemjee || 0) +
+                (khariltsagchiinGaralt?.khuudasniiDugaar || 0) *
+                  (khariltsagchiinGaralt?.khuudasniiKhemjee || 0) -
+                (khariltsagchiinGaralt?.khuudasniiKhemjee || 0) +
                 index +
                 1,
             },
-            { title: "Ангилал", dataIndex: "angilal", ellipsis: true },
-            { title: "Нэр", dataIndex: "ner", ellipsis: true },
-            { title: "Хаяг", dataIndex: "khayag", ellipsis: true },
-            { title: "Утас", dataIndex: "utas", ellipsis: true },
-            { title: "Мэйл хаяг", dataIndex: "email", ellipsis: true },
-            { title: "Повьлон тоо", dataIndex: "too", ellipsis: true },
             {
               title: "Төрөл",
               dataIndex: "turul",
               ellipsis: true,
+              render: (turul) => {
+                return (
+                  <Tag color={turul === "Иргэн" ? "blue" : "orange"}>
+                    {turul}
+                  </Tag>
+                )
+              },
+            },
+            { title: "Регистр", dataIndex: "register", ellipsis: true },
+            { title: "Нэр", dataIndex: "ner", ellipsis: true },
+            { title: "Хаяг", dataIndex: "khayag", ellipsis: true },
+            { title: "Утас", dataIndex: "utas", ellipsis: true },
+            { title: "И-мэйл хаяг", dataIndex: "mail", ellipsis: true },
+
+            {
+              title: "Төлөв",
+              dataIndex: "tuluv",
+              ellipsis: true,
+              render: () => {
+                return <Tag color="green">Идэвхтэй</Tag>
+              },
             },
             {
               title: "Түүх",
               dataIndex: "turul",
               ellipsis: true,
+              render: () => (
+                <a className="ant-dropdown-link p-2 rounded-full hover:bg-gray-200 flex items-center justify-center">
+                  <FileTextOutlined style={{ fontSize: "18px" }} />
+                </a>
+              ),
             },
             {
               title: "Бүртгэгдсэн",
               dataIndex: "createdAt",
               ellipsis: true,
+              render: (data) => {
+                return moment(data).format("YYYY-MM-DD")
+              },
             },
 
             // {
@@ -449,10 +489,10 @@ function AjiltanBurtgel({ token }) {
                       <EditOutlined style={{ fontSize: "18px" }} />
                     </a>
                     <Popconfirm
-                      title="Ажилтан устгах уу?"
+                      title="Харилцагч устгах уу?"
                       okText="Тийм"
                       cancelText="Үгүй"
-                      onConfirm={() => ajiltanUstgay(data)}
+                      onConfirm={() => khariltsagchUstgay(data)}
                     >
                       <a className="ant-dropdown-link p-2 rounded-full hover:bg-gray-200 flex items-center justify-center">
                         <DeleteOutlined
