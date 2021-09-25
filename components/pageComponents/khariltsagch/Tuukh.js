789b77d3a8b4788c3f76c8d69a2a74bb0a0b6347
baterdenets <bbbatuk99@gmail.com>
2021-09-25 18:23:15 +0800

    zasvar

--- components/pageComponents/khariltsagch/Tuukh.js
+++ components/pageComponents/khariltsagch/Tuukh.js
@@ -1,6 +1,7 @@
 import useGereeniiJagsaalt from 'hooks/useGereeniiJagsaalt'
 import React from 'react'
 import moment from 'moment'
+import formatNumber from 'tools/function/formatNumber'
 
 function Tuukh({destroy,data,token},ref) {
 
@@ -18,10 +19,10 @@ function Tuukh({destroy,data,token},ref) {
     )
 
     return (
-        <div>
+        <div className='space-y-2'>
             {gereeniiMedeelel?.jagsaalt?.map((a)=>
-            <div key={a._id}>
-                <div className='w-full flex flex-row'>
+            <div key={a._id} className='p-2 shadow-lg rounded-lg divide-y'>
+                <div className='w-full flex flex-row '>
                     <div className='w-1/2'>Талбайн дугаар</div>
                     <div className='w-1/2'>{a.talbainiiDugaar}</div>
                 </div>
@@ -34,8 +35,32 @@ function Tuukh({destroy,data,token},ref) {
                     <div className='w-1/2'>{moment(a.duusakhOgnoo).format('YYYY-MM-DD')}</div>
                 </div>
                 <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Талбайн дугаар</div>
-                    <div className='w-1/2'>{a.talbainiiDugaar}</div>
+                    <div className='w-1/2'>Гэрээ байгуулсан хугацаа</div>
+                    <div className='w-1/2'>{a.khugatsaa}</div>
+                </div>
+                <div className='w-full flex flex-row'>
+                    <div className='w-1/2'>Түрээсийн төлбөр / сар</div>
+                    <div className='w-1/2'>{formatNumber(a.sariinTurees)}₮</div>
+                </div>
+                <div className='w-full flex flex-row'>
+                    <div className='w-1/2'>Талбайн хөрөнгө</div>
+                    <div className='w-1/2'>{formatNumber(a.sariinTurees)}₮</div>
+                </div>
+                <div className='w-full flex flex-row'>
+                    <div className='w-1/2'>Хөнгөлөлт хоног</div>
+                    <div className='w-1/2'>{formatNumber(a.khungulukhKhugatsaa)}</div>
+                </div>
+                <div className='w-full flex flex-row'>
+                    <div className='w-1/2'>Хөнгөлсөн дүн</div>
+                    <div className='w-1/2'>{formatNumber(a.sariinTurees)}₮</div>
+                </div>
+                <div className='w-full flex flex-row'>
+                    <div className='w-1/2'>Барьцаа төлбөр</div>
+                    <div className='w-1/2'>{formatNumber(a.baritsaaAvakhDun)}₮</div>
+                </div>
+                <div className='w-full flex flex-row'>
+                    <div className='w-1/2'>Чиглэл</div>
+                    <div className='w-1/2'></div>
                 </div>
             </div>)}
         </div>
