6f8f7ec6389b6abe7bb0a882b4925e3fae25dbd0
sumkaMA <sumkakh@gmail.com>
2021-10-02 12:54:36 +0800

    shinechlelt

--- components/pageComponents/khariltsagch/Tuukh.js
+++ components/pageComponents/khariltsagch/Tuukh.js
@@ -1,70 +1,74 @@
-import useGereeniiJagsaalt from 'hooks/useGereeniiJagsaalt'
-import React from 'react'
-import moment from 'moment'
-import formatNumber from 'tools/function/formatNumber'
+import useGereeniiJagsaalt from "hooks/useGereeniiJagsaalt"
+import React from "react"
+import moment from "moment"
+import formatNumber from "tools/function/formatNumber"
 
-function Tuukh({destroy,data,token},ref) {
+function Tuukh({ destroy, data, token }, ref) {
+  const { gereeniiMedeelel, gereeniiMedeelelMutate, setGereeniiKhuudaslalt } =
+    useGereeniiJagsaalt(token, data?.baiguullagiinId, data?.register)
 
-    const { gereeniiMedeelel, gereeniiMedeelelMutate, setGereeniiKhuudaslalt } =
-    useGereeniiJagsaalt(token, data?.baiguullagiinId,data?.register)
+  React.useImperativeHandle(
+    ref,
+    () => ({
+      khaaya() {
+        destroy()
+      },
+    }),
+    []
+  )
 
-    React.useImperativeHandle(
-        ref,
-        () => ({
-          khaaya() {
-            destroy()
-          },
-        }),
-        []
-    )
-
-    return (
-        <div className='space-y-2'>
-            {gereeniiMedeelel?.jagsaalt?.map((a)=>
-            <div key={a._id} className='p-2 shadow-lg rounded-lg divide-y'>
-                <div className='w-full flex flex-row '>
-                    <div className='w-1/2'>Талбайн дугаар</div>
-                    <div className='w-1/2'>{a.talbainiiDugaar}</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Эхэлсэн</div>
-                    <div className='w-1/2'>{moment(a.gereeniiOgnoo).format('YYYY-MM-DD')}</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Талбайн дугаар</div>
-                    <div className='w-1/2'>{moment(a.duusakhOgnoo).format('YYYY-MM-DD')}</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Гэрээ байгуулсан хугацаа</div>
-                    <div className='w-1/2'>{a.khugatsaa}</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Түрээсийн төлбөр / сар</div>
-                    <div className='w-1/2'>{formatNumber(a.sariinTurees)}₮</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Талбайн хөрөнгө</div>
-                    <div className='w-1/2'>{formatNumber(a.sariinTurees)}₮</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Хөнгөлөлт хоног</div>
-                    <div className='w-1/2'>{formatNumber(a.khungulukhKhugatsaa)}</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Хөнгөлсөн дүн</div>
-                    <div className='w-1/2'>{formatNumber(a.sariinTurees)}₮</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Барьцаа төлбөр</div>
-                    <div className='w-1/2'>{formatNumber(a.baritsaaAvakhDun)}₮</div>
-                </div>
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/2'>Чиглэл</div>
-                    <div className='w-1/2'></div>
-                </div>
-            </div>)}
+  return (
+    <div className="space-y-2">
+      {gereeniiMedeelel?.jagsaalt?.map((a) => (
+        <div key={a._id} className="p-2 shadow-lg rounded-lg divide-y">
+          <div className="w-full flex flex-row ">
+            <div className="w-1/2">Талбайн дугаар</div>
+            <div className="w-1/2">{a.talbainDugaar}</div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Эхэлсэн</div>
+            <div className="w-1/2">
+              {moment(a.gereeniiOgnoo).format("YYYY-MM-DD")}
+            </div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Талбайн дугаар</div>
+            <div className="w-1/2">
+              {moment(a.duusakhOgnoo).format("YYYY-MM-DD")}
+            </div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Гэрээ байгуулсан хугацаа</div>
+            <div className="w-1/2">{a.khugatsaa}</div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Түрээсийн төлбөр / сар</div>
+            <div className="w-1/2">{formatNumber(a.sariinTurees)}₮</div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Талбайн хөрөнгө</div>
+            <div className="w-1/2">{formatNumber(a.sariinTurees)}₮</div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Хөнгөлөлт хоног</div>
+            <div className="w-1/2">{formatNumber(a.khungulukhKhugatsaa)}</div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Хөнгөлсөн дүн</div>
+            <div className="w-1/2">{formatNumber(a.sariinTurees)}₮</div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Барьцаа төлбөр</div>
+            <div className="w-1/2">{formatNumber(a.baritsaaAvakhDun)}₮</div>
+          </div>
+          <div className="w-full flex flex-row">
+            <div className="w-1/2">Чиглэл</div>
+            <div className="w-1/2"></div>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default React.forwardRef(Tuukh)
--- pages/khyanalt/geree/gereeBurtgel/index.js
+++ pages/khyanalt/geree/gereeBurtgel/index.js
@@ -12,7 +12,15 @@ import {
   EditOutlined,
   DeleteOutlined,
 } from "@ant-design/icons"
-import { Table, Card, Popover, Badge, Popconfirm, Drawer, DatePicker } from "antd"
+import {
+  Table,
+  Card,
+  Popover,
+  Badge,
+  Popconfirm,
+  Drawer,
+  DatePicker,
+} from "antd"
 import { toWords } from "mon_num"
 import Admin from "components/Admin"
 import formatNumber from "tools/function/formatNumber"
@@ -24,7 +32,7 @@ import deleteMethod from "tools/function/crud/deleteMethod"
 import GereeKharakh from "components/pageComponents/geree/Kharakh"
 import router from "next/router"
 import { useReactToPrint } from "react-to-print"
-import locale from 'antd/lib/date-picker/locale/mn_MN'
+import locale from "antd/lib/date-picker/locale/mn_MN"
 
 function ZakhialgiinKhyanalt() {
   const { token, baiguullaga } = useAuth()
@@ -99,7 +107,7 @@ function ZakhialgiinKhyanalt() {
       },
       {
         title: "Талбай",
-        dataIndex: "talbainiiDugaar",
+        dataIndex: "talbainDugaar",
         ellipsis: true,
       },
       {
@@ -397,7 +405,7 @@ function ZakhialgiinKhyanalt() {
             )
           })}
         </div>
-        <div className='mt-5'>
+        <div className="mt-5">
           <DatePicker.RangePicker locale={locale} />
         </div>
         <div className="overflow-auto hidden md:block mt-8">
