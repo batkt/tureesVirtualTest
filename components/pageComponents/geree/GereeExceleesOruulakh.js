f3b0fc58a353d384eed58520f5e236396d171a99
baterdenets <bbbatuk99@gmail.com>
2021-10-02 14:45:36 +0800

    saijruulalt

---
+++ components/pageComponents/geree/GereeExceleesOruulakh.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { message, Select, Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import { url } from "services/uilchilgee";
+import useGereeniiZagvar from 'hooks/useGereeniiZagvar'
+import _ from "lodash";
+
+function GereeExceleesOruulakh(
+  { token, destroy, zam, garchig, tailbar, zagvariinZam, onFinish ,baiguullaga},
+  ref
+) {
+
+  const [zagvariinId,setGereeniiZagvar] = React.useState(null)
+
+  const { gereeniiZagvarGaralt, setGereeniiZagvarKhuudaslalt } =
+    useGereeniiZagvar(token, baiguullaga?._id);
+
+  React.useImperativeHandle(
+    ref,
+    () => ({
+      khaaya() {
+        _.isFunction(onFinish) && onFinish();
+        destroy();
+      },
+    }),
+    []
+  );
+
+  return (
+    <div>
+      <Select placeholder='Гэрээний загвар' onChange={setGereeniiZagvar} style={{width:'100%'}}>
+        {gereeniiZagvarGaralt?.jagsaalt?.map(a=><Select.Option value={a._id}>{a.ner}</Select.Option>)}
+      </Select>
+      <div className='mt-5'/>
+      <Upload
+        type="drag"
+        multiple={false}
+        name="file"
+        data={{zagvariinId}}
+        action={`${url}/${zam}`}
+        method="POST"
+        headers={{ Authorization: `bearer ${token}` }}
+        beforeUpload={(file)=>{
+          if(!zagvariinId){
+            message.warning('Гэрээний загвар сонгоно уу')
+            return false
+          }
+          return file
+        }}
+        onChange={({ file }) => {
+          console.log(file.response)
+          if (file.response === "Amjilttai") {
+            message.success("Гэрээний заалт Excel -ээс амжилттай орууллаа");
+            _.isFunction(onFinish) && onFinish();
+            destroy();
+          }
+          else if(!!file.response?.aldaa)
+          message.warning(file.response?.aldaa);
+        }}
+      >
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">{garchig}</p>
+        <p className="ant-upload-hint">{tailbar}</p>
+      </Upload>
+      {zagvariinZam && (
+        <a
+          className="cursor-pointer text-blue-600 font-medium"
+          href={url + `/${zagvariinZam}`}
+          download
+        >
+          Загвар татах
+        </a>
+      )}
+    </div>
+  );
+}
+
+export default React.forwardRef(GereeExceleesOruulakh);
--- pages/khyanalt/geree/gereeBurtgel/index.js
+++ pages/khyanalt/geree/gereeBurtgel/index.js
@@ -12,7 +12,7 @@ import {
   EditOutlined,
   DeleteOutlined,
 } from "@ant-design/icons"
-import { Table, Card, Popover, Badge, Popconfirm, Drawer, DatePicker } from "antd"
+import { Table, Card, Popover, Badge, Popconfirm, Drawer, DatePicker, Button, Space } from "antd"
 import { toWords } from "mon_num"
 import Admin from "components/Admin"
 import formatNumber from "tools/function/formatNumber"
@@ -25,6 +25,8 @@ import GereeKharakh from "components/pageComponents/geree/Kharakh"
 import router from "next/router"
 import { useReactToPrint } from "react-to-print"
 import locale from 'antd/lib/date-picker/locale/mn_MN'
+import GereeExceleesOruulakh from "components/pageComponents/geree/GereeExceleesOruulakh"
+import { modal } from "components/ant/Modal"
 
 function ZakhialgiinKhyanalt() {
   const { token, baiguullaga } = useAuth()
@@ -34,6 +36,7 @@ function ZakhialgiinKhyanalt() {
     useGereeniiJagsaaltToollolt(token)
   const [kharuulakhGeree, setKharuulakhGeree] = React.useState(null)
   const componentRef = React.useRef()
+  const excelref = React.useRef()
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   })
@@ -340,6 +343,38 @@ function ZakhialgiinKhyanalt() {
     )
   }
 
+  function gereeOruulakhExcel() {
+    const footer = [
+      <Space>
+        <Button onClick={() => excelref.current.khaaya()}>Хаах</Button>,
+        <Button
+          style={{ backgroundColor: "#209669", color: "#ffffff" }}
+          onClick={() => excelref.current.khaaya()}
+        >
+          хадгалах
+        </Button>
+        ,
+      </Space>,
+    ];
+    modal({
+      title: "",
+      icon: <FileExcelOutlined />,
+      content: (
+        <GereeExceleesOruulakh
+          ref={excelref}
+          token={token}
+          baiguullaga={baiguullaga}
+          onFinish={gereeniiMedeelelMutate}
+          zam="gereeniiExcelTatya"
+          garchig="Excel файл аа чирч оруулах эсвэл сонгоно уу"
+          tailbar="Харилцагч загварын excel файл"
+          zagvariinZam="gereeniiExcelAvya"
+        />
+      ),
+      footer,
+    });
+  }
+
   return (
     <Admin
       khuudasniiNer="gereeBurtgel"
@@ -397,8 +432,22 @@ function ZakhialgiinKhyanalt() {
             )
           })}
         </div>
-        <div className='mt-5'>
+        <div className='mt-5 flex flex-row'>
           <DatePicker.RangePicker locale={locale} />
+          <div className='ml-auto'>
+            <Button
+              style={{
+                alignItems: "end",
+                backgroundColor: "#209669",
+                color: "#ffffff",
+                display: "flex",
+              }}
+              icon={<FileExcelOutlined style={{ fontSize: "16px" }} />}
+              onClick={gereeOruulakhExcel}
+            >
+              Excel -ээс Гэрээ татах
+            </Button>
+          </div>
         </div>
         <div className="overflow-auto hidden md:block mt-8">
           <Table
