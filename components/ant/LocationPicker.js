6a07bdeb0ab046450fe2e70ebc4540180caa69f5
baterdenets <bbbatuk99@gmail.com>
2021-11-10 19:10:57 +0800

    zasvar

---
+++ components/ant/LocationPicker.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import GazriinZurag from 'components/gazriinZurag/GazriinZurag'
+import { Marker } from '@react-google-maps/api'
+function LocationPicker({value,onChange}) {
+    const [position, setPosition] = React.useState({lat:value?.coordinates[0],lng:value?.coordinates[0]})
+    function onDragEnd(e) {
+        setPosition({ lat: e.latLng.lat(), lng: e.latLng.lng() })
+        onChange({ type: 'Point', coordinates: [e.latLng.lat(), e.latLng.lng()] })
+    }
+
+    return (
+        <div className='w-full h-32'>
+             <GazriinZurag
+                center={{ lat: position?.lat || 47.927094, lng: position?.lng || 106.887425 }}
+                zoom={13}
+                options={{
+                    fullscreenControlOptions: { position: 12 },
+                    disableDoubleClickZoom: true,
+                    fullscreenControl: true,
+                    zoomControl: false,
+                    mapTypeControl: false,
+                    streetViewControl: false,
+                }}
+            >
+                <Marker
+                    draggable
+                    onDragEnd={onDragEnd}
+                    position={{ lat: position?.lat || 47.927094, lng: position?.lng || 106.887425 }}
+                />
+            </GazriinZurag>   
+        </div>
+    )
+}
+
+export default LocationPicker
