a3cc9425d57d59c8676ff88cd1b2577cc47cd55a
baterdenets <bbbatuk99@gmail.com>
2021-11-25 12:15:24 +0800

    zasvar

--- pages/khyanalt/tokhirgoo/index.js
+++ pages/khyanalt/tokhirgoo/index.js
@@ -48,7 +48,7 @@ function AjiltanBurtgel({ token }) {
           tsonkh:<AshiglaltiinZardal {...{ajiltan, ajiltanMutate,baiguullaga,baiguullagaMutate}} token={token}/>
         },
         {
-          icon:<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-settings w-4 h-4 mr-2"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg> ,
+          icon:<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-settings w-4 h-4 mr-2"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/></svg>,
           text:'Мэдэгдэл',
           tsonkh:<Medegdel {...{ajiltan, ajiltanMutate,baiguullaga,baiguullagaMutate}} token={token}/>
         }
