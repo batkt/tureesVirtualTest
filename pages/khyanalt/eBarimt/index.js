22a67cacbc6cff910281a6a404da2b2524020f25
baterdenets <bbbatuk99@gmail.com>
2021-12-10 13:12:24 +0800

    zasvar

--- pages/khyanalt/eBarimt/index.js
+++ pages/khyanalt/eBarimt/index.js
@@ -29,6 +29,7 @@ function EbarimtMedeelel({ token }) {
 
   const query = useMemo(() => {
     return {
+      $or:[{ustgasanOgnoo:null},{ustgasanOgnoo:{$exists:false}}],
       createdAt: {
         $gte: moment(ekhlekhOgnoo[0]).format("YYYY-MM-DD 00:00:00"),
         $lte: moment(ekhlekhOgnoo[1]).format("YYYY-MM-DD 23:59:59"),
