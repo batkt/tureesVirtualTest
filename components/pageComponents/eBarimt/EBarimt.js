62eae2b6d835e269d515203228e4128c5c751064
sumkaMA <sumkakh@gmail.com>
2021-12-22 20:53:33 +0800

    shinechlelt

--- components/pageComponents/eBarimt/EBarimt.js
+++ components/pageComponents/eBarimt/EBarimt.js
@@ -45,7 +45,7 @@ function EBarimt({
             />
           </div>
         </div>
-        {barimtKhevlekhEsekh && !irgenEsekh && (
+        {
           <div className="flex flex-row border-b-2 border-dashed py-2">
             <div>ААН эсэх</div>
             <div className="ml-auto">
@@ -55,15 +55,15 @@ function EBarimt({
               />
             </div>
           </div>
-        )}
-        {barimtKhevlekhEsekh && !baiguullagaEsekh && (
+        }
+        {
           <div className="flex flex-row border-b-2 border-dashed py-2">
             <div>Иргэнд эсэх</div>
             <div className="ml-auto">
               <Switch checked={irgenEsekh} onChange={setIrgenEsekh} />
             </div>
           </div>
-        )}
+        }
         {baiguullagaEsekh && (
           <div className="flex flex-row border-b-2 border-dashed py-2">
             <div>ААН регистр</div>
