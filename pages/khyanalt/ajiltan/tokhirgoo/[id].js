734ca014b01afe2ac29ff7326015c28c1964bdae
sumkaMA <sumkakh@gmail.com>
2022-01-25 21:16:35 +0800

    shinechlelt

--- pages/khyanalt/ajiltan/tokhirgoo/[id].js
+++ pages/khyanalt/ajiltan/tokhirgoo/[id].js
@@ -1,46 +1,61 @@
-import { Button, message, Select, Switch, Tooltip, Transfer } from "antd";
-import Admin from "components/Admin";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import shalgaltKhiikh from "services/shalgaltKhiikh";
-import uilchilgee from "services/uilchilgee";
-import readMethod from "tools/function/crud/readMethod";
-import {tsonknuud,khereglegchiinErkhuud} from 'tools/logic/khereglegchiinErkhiinTokhirgoo'
-
+import { Button, message, Select, Switch, Tooltip, Transfer } from "antd"
+import Admin from "components/Admin"
+import { useRouter } from "next/router"
+import React, { useEffect, useState } from "react"
+import shalgaltKhiikh from "services/shalgaltKhiikh"
+import uilchilgee from "services/uilchilgee"
+import readMethod from "tools/function/crud/readMethod"
+import {
+  tsonknuud,
+  khereglegchiinErkhuud,
+} from "tools/logic/khereglegchiinErkhiinTokhirgoo"
+import { useAuth } from "services/auth"
+import useAjiltan from "hooks/useAjiltan"
 
 function index({ token, data }) {
   const router = useRouter()
-  const [targetKeys, setTargetKeys] = useState(data?.tsonkhniiErkhuud || []);
-  const [selectedKeys, setSelectedKeys] = useState([]);
-  const [tokhirgoo, setTokhirgoo] = useState([]);
-  const [khiikhTokhirgoo, setkhiikhTokhirgoo] = useState([]);
-
+  const [targetKeys, setTargetKeys] = useState(data?.tsonkhniiErkhuud || [])
+  const { ajiltan, ajiltanMutate } = useAjiltan(token)
+  const [selectedKeys, setSelectedKeys] = useState([])
+  const [tokhirgoo, setTokhirgoo] = useState([])
+  const [khiikhTokhirgoo, setkhiikhTokhirgoo] = useState([])
+  const { baiguullaga, barilgiinId } = useAuth()
+  const [ajiltanMedeelel, setAjiltanMedeelel] = useState()
+  const barilguud = baiguullaga?.barilguud
   const handleChange = (nextTargetKeys, direction, moveKeys) => {
-    setTargetKeys(nextTargetKeys);
+    setTargetKeys(nextTargetKeys)
     setkhiikhTokhirgoo(
-      tsonknuud.filter((a) => !a.nuuya && !!nextTargetKeys.find((b) => b === a.key))
-    );
-  };
+      tsonknuud.filter(
+        (a) => !a.nuuya && !!nextTargetKeys.find((b) => b === a.key)
+      )
+    )
+  }
 
   const handleSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
-    setSelectedKeys([...sourceSelectedKeys, ...targetSelectedKeys]);
-  };
-
+    setSelectedKeys([...sourceSelectedKeys, ...targetSelectedKeys])
+  }
+  
   const khadgalya = () => {
-    uilchilgee(token).post(`/ajiltandErkhUgyu/${data?._id}`,{tsonkhniiErkhuud:targetKeys})
-    .then(({data})=>{
-      if(data === 'Amjilttai'){
-        message.success("Бүртгэл амжилттай хийгдлээ");
-        router.back()
-      }
-
-    })
+    uilchilgee(token)
+      .post(`/ajiltandErkhUgyu/${data?._id}`, { tsonkhniiErkhuud: targetKeys,barilguud:ajiltanMedeelel })
+      .then(({ data }) => {
+        if (data === "Amjilttai") {
+          message.success("Бүртгэл амжилттай хийгдлээ")
+          router.back()
+        }
+      })
   }
 
   const erkhSoliyo = (erkh) => {
-    const khereglegchiinErkh = khereglegchiinErkhuud.find(a=>a.erkh === erkh)
+    const khereglegchiinErkh = khereglegchiinErkhuud.find(
+      (a) => a.erkh === erkh
+    )
     setTargetKeys([...khereglegchiinErkh.tsonkhnuud])
   }
+  useEffect(() => {
+    setAjiltanMedeelel(ajiltan?.barilguud)
+  }, [ajiltan])
+  
 
   return (
     <Admin title={"Ажилтны эрхийн тохиргоо"} dedKhuudas className="p-5">
@@ -55,18 +70,34 @@ function index({ token, data }) {
           </div>
         </div>
         <div className="mx-3">
-            <Select onChange={erkhSoliyo} placeholder='Хэрэглэгчийн эрх'>
-              {
-                khereglegchiinErkhuud.map(a=>(
-                  <Select.Option key={a.erkh} value={a.erkh}>
-                    {a.tailbar}                    
-                  </Select.Option>
-                ))
-              }
-            </Select>
-          </div>
+          <Select onChange={erkhSoliyo} placeholder="Хэрэглэгчийн эрх">
+            {khereglegchiinErkhuud.map((a) => (
+              <Select.Option key={a.erkh} value={a.erkh}>
+                {a.tailbar}
+              </Select.Option>
+            ))}
+          </Select>
+        </div>
+        <div className="flex flex-row space-x-3 ml-4">
+          <div>Барилга тохируулах</div>
+          <Select
+            mode="multiple"            
+            onChange={(v) => setAjiltanMedeelel(v)}
+            placeholder="Барилга тохируулах"
+            style={{ width: "80%" }}
+            value={ajiltanMedeelel}
+          >
+            {barilguud?.map((a) => (
+              <Select.Option key={a._id} value={a._id}>
+                {a.ner}
+              </Select.Option>
+            ))}
+          </Select>
+        </div>
         <div className="ml-auto mr-2">
-          <Button type="primary" onClick={khadgalya}>Хадгалах</Button>
+          <Button type="primary" onClick={khadgalya}>
+            Хадгалах
+          </Button>
         </div>
       </div>
       <div className="box col-span-6 p-2">
@@ -77,14 +108,14 @@ function index({ token, data }) {
         </div>
         <Transfer
           locale={{
-            itemsUnit: 'Боломжит эрх',
-            remove: 'Буцаах',
-            selectAll: 'Бүгдийг сонгох',
-            selectInvert: 'Эсэргээр нь сонгох',
-            removeAll: 'Бүгдийг буцаах',
+            itemsUnit: "Боломжит эрх",
+            remove: "Буцаах",
+            selectAll: "Бүгдийг сонгох",
+            selectInvert: "Эсэргээр нь сонгох",
+            removeAll: "Бүгдийг буцаах",
           }}
           listStyle={{ height: "100%", width: "45%", marginTop: 10 }}
-          dataSource={tsonknuud.filter(a=>!a.nuuya)}
+          dataSource={tsonknuud.filter((a) => !a.nuuya)}
           titles={["Цонхнууд", "Цонхны эрх"]}
           targetKeys={targetKeys}
           selectedKeys={selectedKeys}
@@ -120,11 +151,11 @@ function index({ token, data }) {
                     onChange={(checked) => {
                       setTokhirgoo((a) => {
                         if (!checked) {
-                          const index = a.findIndex((b) => b === mur.ner);
-                          a.splice(index, 1);
-                        } else a.push(mur.ner);
-                        return [...a];
-                      });
+                          const index = a.findIndex((b) => b === mur.ner)
+                          a.splice(index, 1)
+                        } else a.push(mur.ner)
+                        return [...a]
+                      })
                     }}
                   />
                 </div>
@@ -133,7 +164,7 @@ function index({ token, data }) {
           ))}
       </div>
     </Admin>
-  );
+  )
 }
 
 const ugudulAvchirya = async (ctx, session) => {
@@ -141,10 +172,10 @@ const ugudulAvchirya = async (ctx, session) => {
     "ajiltan",
     session.tureestoken,
     ctx.query.id
-  );
-  return data;
-};
+  )
+  return data
+}
 
-export const getServerSideProps = (ctx) => shalgaltKhiikh(ctx, ugudulAvchirya);
+export const getServerSideProps = (ctx) => shalgaltKhiikh(ctx, ugudulAvchirya)
 
-export default index;
+export default index
