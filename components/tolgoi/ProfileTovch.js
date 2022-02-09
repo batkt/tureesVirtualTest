6ae519a2349a4a6da6be73e9b134068ccaf1da29
baterdenets <bbbatuk99@gmail.com>
2022-02-09 11:19:25 +0800

    saijruulalt

--- components/tolgoi/ProfileTovch.js
+++ components/tolgoi/ProfileTovch.js
@@ -117,7 +117,7 @@ function ProfileTovch({ ajiltan, garya, token }) {
         }
       >
         <button className="h-8 w-8 flex rounded-full items-center justify-center focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">
-          <Badge count={sonorduulga?.kharaaguiToo}>
+          <Badge count={sonorduulga?.niitMur} dot>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" 
               strokeLinejoin="round" className="feather feather-bell block mx-auto w-5 h-5"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
           </Badge>
