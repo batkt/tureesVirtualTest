b3fe0ef7467b77aacfa3fe3f9dfb363021dc2b7e
baterdenets <bbbatuk99@gmail.com>
2022-03-11 16:23:39 +0800

    saijruulalt

--- components/pageComponents/zogsool/MashinBurtgel.js
+++ components/pageComponents/zogsool/MashinBurtgel.js
@@ -1,10 +1,13 @@
-import React, { useImperativeHandle } from "react"
+import React, { useImperativeHandle, useState } from "react"
 import { Form, Input, message, Select } from "antd"
 import createMethod from "tools/function/crud/createMethod"
 import updateMethod from "tools/function/crud/updateMethod"
+import uilchilgee from "services/uilchilgee"
 
 function MashinBurtgel({ data, barilgiinId, token, destroy, onRefresh }, ref) {
     const [form] = Form.useForm()
+
+    const [geree,setGeree] = useState()
 
     useImperativeHandle(
       ref,
@@ -12,6 +15,8 @@ function MashinBurtgel({ data, barilgiinId, token, destroy, onRefresh }, ref) {
         khadgalya() {
           const data = form.getFieldsValue()
           data.barilgiinId = barilgiinId
+          data.ezemshigchiinTalbainDugaar = geree.talbainDugaar
+          data.gereeniiDugaar = geree.gereeniiDugaar
           const method = data?._id ? updateMethod : createMethod
           method("mashin", token, data).then(({ data }) => {
             if (data === "Amjilttai") {
@@ -25,8 +30,16 @@ function MashinBurtgel({ data, barilgiinId, token, destroy, onRefresh }, ref) {
           destroy()
         },
       }),
-      [form]
+      [form,geree]
     )
+
+    function gereeAvya({target}) {
+      if(target.value?.length > 7 )
+      uilchilgee(token).post('/utasniiDugaaraarGereeAvya',{utas:target.value}).then(({data})=>{
+        if(!!data)
+          setGeree({...data})
+      })
+    }
   
     return (
       <Form form={form} initialValues={data} className="space-y-2" labelCol={{ span: 6 }} wrapperCol={{ span: 24 }}>
@@ -35,6 +48,9 @@ function MashinBurtgel({ data, barilgiinId, token, destroy, onRefresh }, ref) {
             <Select placeholder="Төрөл" >
                 {['Гэрээт','Түрээслэгч','Дотоод'].map(a=><Select.Option key={a} value={a}>{a}</Select.Option>)}
             </Select>
+        </Form.Item>
+        <Form.Item label="Утас" name="ezemshigchiinUtas">
+            <Input placeholder="Утас" onChange={gereeAvya}/>
         </Form.Item>
         <Form.Item label="Машины дугаар" name="dugaar">
             <Input placeholder="Машины дугаар" />
@@ -45,9 +61,7 @@ function MashinBurtgel({ data, barilgiinId, token, destroy, onRefresh }, ref) {
         <Form.Item label="Регистр" name="ezemshigchiinRegister">
             <Input placeholder="Регистр" />
         </Form.Item>
-        <Form.Item label="Утас" name="ezemshigchiinUtas">
-            <Input placeholder="Утас" />
-        </Form.Item>
+        
       </Form>
     )
 }
