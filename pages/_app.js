b88b0a1f0ccaefa432d2dcb4e5197a1f6abc0de0
baterdenets <bbbatuk99@gmail.com>
2022-06-06 17:45:43 +0800

    saijruulalt

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { AuthProvider } from "../services/auth";
 import { ThemeProvider } from "next-themes";
-import "../styles/globals.css";
 import "antd/dist/antd.css";
+import "../styles/globals.css";
 import "suneditor/dist/css/suneditor.min.css";
 import "aos/dist/aos.css";
 
