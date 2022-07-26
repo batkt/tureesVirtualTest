4c74c914cdebbf0062bf330e832a66df4659afaa
baterdenets <bbbatuk99@gmail.com>
2022-07-26 12:25:33 +0800

    sairjuulalt

--- components/loader/index.js
+++ components/loader/index.js
@@ -1,16 +1,45 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 
 export default function Loader() {
+  useEffect(() => {
+    // window.addEventListener("keydown", (e) => {
+    //   e = e || window.event;
+    //   e.preventDefault();
+    //   return "Sure?";
+    // });
+    window.addEventListener("beforeunload", (e) => {
+      e = e || window.event;
+      e.preventDefault();
+      if (e) {
+        e.returnValue = "Sure?";
+      }
+      return "yahoo";
+    });
+    return () => {
+      // window.removeEventListener("keydown", (e) => {
+      //   e = e || window.event;
+      //   e.preventDefault();
+      //   return "Sure?";
+      // });
+      window.removeEventListener("beforeunload", (e) => {
+        e = e || window.event;
+        if (e) {
+          e.returnValue = "Sure?";
+        }
+        return "yahoo";
+      });
+    };
+  }, []);
+
   return (
-    <div className="bg-white bg-opacity-60 select-none dark:bg-black transition-all dark:bg-opacity-60 flex justify-center items-center w-full h-full absolute z-10">
-      <div className="absolute lg:w-[20vw] w-56 h-56 lg:h-[20vw] self-center justify-self-center flex justify-center items-center bg-gray-200 dark:bg-gray-900 before:animate-spin before:absolute lg:before:w-[20vw] before:w-56 before:h-56 lg:before:h-[20vw] before:bg-gradient-to-b before:from-blue-700 before:rounded-full rounded-full before:via-transparent after:absolute after:inset-1 after:bg-gray-50  after:rounded-full ">
-        <div className="relative h-5 w-56 lg:w-[20vw] animate-spin overflow-visible z-10">
-          <div className="absolute h-6 w-6 rounded-full bg-green-600 lg:-right-[10px] lg:-top-5  top-3" />
+    <div className="absolute z-10 flex h-full w-full select-none items-center justify-center bg-white bg-opacity-60 transition-all dark:bg-black dark:bg-opacity-60">
+      <div className="absolute flex h-56 w-56 items-center justify-center self-center justify-self-center rounded-full bg-gray-200 before:absolute before:h-56 before:w-56 before:animate-spin before:rounded-full before:bg-gradient-to-b before:from-blue-700 before:via-transparent after:absolute after:inset-1 after:rounded-full after:bg-gray-50 dark:bg-gray-900 lg:h-[20vw] lg:w-[20vw] lg:before:h-[20vw]  lg:before:w-[20vw] ">
+        <div className="relative z-10 h-5 w-56 animate-spin overflow-visible lg:w-[20vw]">
+          <div className="absolute top-3 h-6 w-6 rounded-full bg-green-600 lg:-right-[10px]  lg:-top-5" />
         </div>
       </div>
-      <div className="lg:h-[15vw] p-2 w-44 h-44 lg:w-[15vw] flex justify-center items-center bg-white dark:bg-gray-800 overflow-hidden shadow-xl z-20 rounded-full">
-        <img  src="/rent.ico" />
+      <div className="z-20 flex h-44 w-44 items-center justify-center overflow-hidden rounded-full bg-white p-2 shadow-xl dark:bg-gray-800 lg:h-[15vw] lg:w-[15vw]">
+        <img src="/rent.ico" draggable={false} />
       </div>
     </div>
   );
