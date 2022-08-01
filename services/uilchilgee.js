37d5a6ab47b2fda44b737c6af644236e49871407
baterdenets <bbbatuk99@gmail.com>
2022-08-01 21:48:12 +0800

    zasvar

--- services/uilchilgee.js
+++ services/uilchilgee.js
@@ -1,7 +1,7 @@
-import { notification } from "antd"
-import axios from "axios"
-import socketIOClient from "socket.io-client"
-import _ from "lodash"
+import { notification } from "antd";
+import axios from "axios";
+import socketIOClient from "socket.io-client";
+import _ from "lodash";
 
 // function hostnameAvya() {
 //   return global.window?.location?.hostname
@@ -10,18 +10,25 @@ import _ from "lodash"
 //export const url = `http://${hostnameAvya() || 'localhost'}:8081`;
 //export const url = `http://localhost:8081`;
 //export const url = "http://192.168.0.103:8081"
-export const url = "http://103.50.205.33:8081"
+export const url = "http://103.143.40.230:8081";
 
-export const socket = () => socketIOClient(url, { transports: ["websocket"] })
+export const socket = () => socketIOClient(url, { transports: ["websocket"] });
 
 export const aldaaBarigch = (e) => {
   if (e?.response?.data?.aldaa === "jwt expired") {
-    window.location.href = "/"
-  } else if (!!e?.response?.data?.aldaa) notification.error({description:e?.response?.data?.aldaa,message:'Алдаа'})
+    window.location.href = "/";
+  } else if (!!e?.response?.data?.aldaa)
+    notification.error({
+      description: e?.response?.data?.aldaa,
+      message: "Алдаа",
+    });
   else if (!!e?.response?.errors)
-    notification.error({description:JSON.stringify(e?.response?.errors),message:'Алдаа'})
-  else notification.error({description:JSON.stringify(e),message:'Алдаа'})
-}
+    notification.error({
+      description: JSON.stringify(e?.response?.errors),
+      message: "Алдаа",
+    });
+  else notification.error({ description: JSON.stringify(e), message: "Алдаа" });
+};
 
 /*axios.interceptors.response.use(function (response) {
     // Do something with response data
@@ -34,12 +41,12 @@ export const aldaaBarigch = (e) => {
 const uilchilgee = (token) => {
   const headers = {
     "Content-type": "application/json",
-  }
-  if (!!token) headers["Authorization"] = `bearer ${token}`
+  };
+  if (!!token) headers["Authorization"] = `bearer ${token}`;
   return axios.create({
     baseURL: url,
     headers,
-  })
-}
+  });
+};
 
-export default uilchilgee
+export default uilchilgee;
