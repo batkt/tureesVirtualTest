e07b7cb737e4e210f5abacf6605c68ad7c48d08a
baterdenets <bbbatuk99@gmail.com>
2022-08-05 15:53:58 +0800

    zasvar

---
+++ components/pageComponents/tulbur/GuilgeeNiiluulekh.js
@@ -0,0 +1,396 @@
+import _ from "lodash";
+import React, { useMemo, useState } from "react";
+import moment from "moment";
+import formatNumber from "tools/function/formatNumber";
+import { Divider, notification, Popconfirm, Popover } from "antd";
+import useGereeniiJagsaalt from "hooks/useGereeniiJagsaalt";
+import { formatter, parser } from "tools/function/inputFormatter";
+import { CloseCircleOutlined } from "@ant-design/icons";
+import uilchilgee, { aldaaBarigch } from "services/uilchilgee";
+
+var timeout = null;
+
+function guilgeeBurduulya(gereenuud, dans, guilgee) {
+  let baritsaa = [];
+  let undsenGuilgee = [];
+  let aldaa = [];
+  gereenuud.forEach((mur) => {
+    if (mur.tureesiinTulbur > 0) {
+      let guilgeeniiMur = {
+        turul: "bank",
+        tulsunDun: mur.tureesiinTulbur,
+        guilgeeniiId: guilgee._id,
+        gereeniiId: mur._id,
+        dansniiDugaar: guilgee.dansniiDugaar,
+      };
+
+      switch (dans.bank) {
+        case "tdb":
+          guilgeeniiMur.ognoo = guilgee.TxDt;
+          guilgeeniiMur.tulsunDans = guilgee.CtAcntOrg;
+          break;
+        case "khanbank":
+          guilgeeniiMur.ognoo = guilgee.tranDate;
+          guilgeeniiMur.tulsunDans = guilgee.relatedAccount;
+          break;
+        default:
+          aldaa.push(
+            `${mur.talbainDugaar} талбайн холбох гүйлгээний данс тодорхоогүй байна`
+          );
+          break;
+      }
+      undsenGuilgee.push(guilgeeniiMur);
+    }
+    if (mur.baritsaaTulbur > 0) {
+      let baritsaaniiMur = {
+        gereeniiId: mur._id,
+        guilgeeniiId: guilgee._id,
+        orlogo: mur.baritsaaTulbur,
+        zarlaga: 0,
+      };
+      switch (dans.bank) {
+        case "tdb":
+          baritsaaniiMur.ognoo = guilgee.TxDt;
+          break;
+        case "khanbank":
+          baritsaaniiMur.ognoo = guilgee.tranDate;
+          break;
+        default:
+          break;
+      }
+      if (mur.baritsaaniiUldegdel < mur.baritsaaTulbur)
+        aldaa.push(
+          `${mur.talbainDugaar} талбайн холбох гүйлгээний барьцааны дүн хэтэрсэн байна`
+        );
+      baritsaa.push(baritsaaniiMur);
+    }
+  });
+
+  return {
+    baritsaa,
+    undsenGuilgee,
+    aldaa,
+  };
+}
+
+async function baritsaaniiGuilgeeKhiiya(token, guilgeenuud) {
+  let aldaa = [];
+  for await (const geree of guilgeenuud) {
+    const khariu = await uilchilgee(token)
+      .post("/baritsaaniiGuilgeeKhiie", geree)
+      .then(({ data }) => data)
+      .catch((e) => {
+        aldaaBarigch(e);
+        aldaa.push(`${e.message}`);
+      });
+    if (khariu !== "Amjilttai") break;
+    break;
+  }
+  return { aldaa };
+}
+
+function GuilgeeNiiluulekh(
+  { data, dans, token, baiguullagiinId, destroy, onFinish },
+  ref
+) {
+  const [gereenuud, setGereenuud] = useState([]);
+  const [visible, setVisible] = useState(false);
+
+  const { gereeniiMedeelel, setGereeniiKhuudaslalt } = useGereeniiJagsaalt(
+    token,
+    baiguullagiinId,
+    undefined,
+    undefined,
+    undefined,
+    10
+  );
+
+  React.useImperativeHandle(
+    ref,
+    () => ({
+      khaaya() {
+        destroy();
+      },
+      async khadgalya() {
+        const { aldaa, baritsaa, undsenGuilgee } = guilgeeBurduulya(
+          gereenuud,
+          dans,
+          data
+        );
+        if (aldaa.length > 0) {
+          notification.warning({
+            message: "Анхаар",
+            description: aldaa.join(","),
+          });
+          return;
+        }
+        if (baritsaa.length > 0) {
+          const baritsaaniiGuilgee = await baritsaaniiGuilgeeKhiiya(
+            token,
+            baritsaa
+          );
+          if (baritsaaniiGuilgee.aldaa.length > 0) {
+            notification.warning({
+              message: "Анхаар",
+              description: baritsaaniiGuilgee.aldaa.join(","),
+            });
+            return;
+          }
+          if (undsenGuilgee.length === 0) {
+            notification.success({
+              message: "Амжилттай",
+              description: "Гүйлгээ амжилттай холбогдлоо",
+            });
+            _.isFunction(onFinish) && onFinish;
+            destroy();
+          }
+        }
+        if (undsenGuilgee.length > 0)
+          uilchilgee(token)
+            .post("/tulultOlnoorKhadgalya", { guilgeenuud: undsenGuilgee })
+            .then(({ data }) => {
+              if (data === "Amjilttai") {
+                notification.success({
+                  message: "Амжилттай",
+                  description: "Гүйлгээ амжилттай холбогдлоо",
+                });
+                _.isFunction(onFinish) && onFinish;
+                destroy();
+              }
+            })
+            .catch(aldaaBarigch);
+      },
+    }),
+    [gereenuud]
+  );
+
+  const content = useMemo(
+    () => (
+      <div className="relative w-72 space-y-1">
+        <div
+          onClick={() => setVisible(false)}
+          className="absolute right-0 -top-10 text-xl"
+        >
+          <CloseCircleOutlined />
+        </div>
+        {gereeniiMedeelel?.jagsaalt?.map((mur, i) => (
+          <div
+            className="grid cursor-pointer grid-cols-3 rounded-md border border-gray-400 p-1 hover:bg-gray-100"
+            key={`gereeniisongolt${i}`}
+            onClick={() => {
+              if (gereenuud.find((a) => a._id === mur._id)) {
+                notification.warning({
+                  message: "Анхаар",
+                  description: "Гэрээ сонгогдсон байна",
+                });
+                return;
+              }
+              setGereenuud((a) => {
+                a.push(mur);
+                return [...a];
+              });
+              setVisible(false);
+            }}
+          >
+            <div>{mur.talbainDugaar}</div>
+            <div>{mur.gereeniiDugaar}</div>
+            <div>{mur.ner}</div>
+          </div>
+        ))}
+      </div>
+    ),
+    [gereeniiMedeelel, gereenuud]
+  );
+
+  function zuruuZun(index, talbar) {
+    let sum = gereenuud.reduce((a, b, currentIndex) => {
+      let value = 0;
+      if (currentIndex === index) {
+        value +=
+          b[
+            talbar === "baritsaaTulbur" ? "tureesiinTulbur" : "baritsaaTulbur"
+          ] || 0;
+      } else {
+        value += b.baritsaaTulbur || 0;
+        value += b.tureesiinTulbur || 0;
+      }
+      return _.toNumber(a + value);
+    }, 0);
+    return sum + (data.kholbosonDun || 0);
+  }
+
+  function onChange({ target }) {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      setGereeniiKhuudaslalt((a) => ({ ...a, search: target.value }));
+    }, 300);
+  }
+
+  const guilgeeniiDun = useMemo(() => {
+    return dans.bank === "tdb" ? data.Amt : data.amount;
+  }, [dans, data]);
+
+  function onChangeKholbokhDun(target, index, talbar) {
+    let sum = zuruuZun(index, talbar);
+    if (sum + _.toNumber(parser(target.value)) > guilgeeniiDun) {
+      target.value = formatter(guilgeeniiDun - sum);
+      notification.warning({
+        message: "Анхаар",
+        description: "Гүйлгээний дүнгээс холбох дүн илүү гарсан байна",
+      });
+    }
+    setGereenuud((a) => {
+      _.set(a, `${index}.${talbar}`, _.toNumber(parser(target.value)));
+      return [...a];
+    });
+  }
+
+  function onDoubleClickKholbokhDun(target, index, talbar) {
+    let sum = zuruuZun(index, talbar);
+    if (sum < guilgeeniiDun) {
+      target.value = formatter(guilgeeniiDun - sum);
+      setGereenuud((a) => {
+        _.set(a, `${index}.${talbar}`, _.toNumber(parser(target.value)));
+        return [...a];
+      });
+    }
+  }
+
+  const zuruuDun = useMemo(() => {
+    let sum = zuruuZun();
+    return guilgeeniiDun - sum;
+  }, [gereenuud]);
+
+  return (
+    <div className="flex w-full flex-col space-y-2">
+      <div className="px-2">
+        <div>Гүйлгээний мэдээлэл</div>
+        <div className="grid w-full grid-cols-4 rounded-md border border-gray-400 bg-gray-100 p-2">
+          <div>{data.CtAcct || data.relatedAccount}</div>
+          <div>{data.CtActnName}</div>
+          <div className="text-center">
+            {moment(data.TxDt || data.tranDate).format("YYYY-MM-DD")}
+          </div>
+          <div className="text-right text-red-600">
+            {formatNumber(guilgeeniiDun)}
+          </div>
+          <div className="col-span-4">{data.TxAddInf || data.description}</div>
+          {data.kholbosonDun > 0 && (
+            <div className="col-span-4 flex justify-between">
+              <span>
+                Холбогдсон талбай:
+                {data.kholbosonTalbainId &&
+                  [...new Set(data.kholbosonTalbainId)].join(",")}
+              </span>
+              <span>Холбогдсон дүн:{formatNumber(data.kholbosonDun || 0)}</span>
+            </div>
+          )}
+        </div>
+        <div>Гүйлгээ холбох</div>
+        <div className="grid grid-cols-2">
+          <Popover
+            placement="bottom"
+            title="Гэрээний жагсаалт"
+            content={content}
+            visible={visible}
+            trigger="click"
+            onVisibleChange={() => setVisible(true)}
+          >
+            <input
+              className="rounded-md border border-gray-400 p-1"
+              placeholder="Гэрээ"
+              onChange={onChange}
+            />
+          </Popover>
+        </div>
+      </div>
+      <div className="h-72 space-y-2 overflow-auto px-2">
+        {gereenuud.map((geree, index) => (
+          <div
+            key={`${index}geree-kholbolt`}
+            className="space-y-2 rounded-md border border-gray-400 p-2"
+          >
+            <div className="flex w-full justify-between text-right text-xl">
+              <span>
+                {geree?.talbainDugaar}-{geree?.ner}
+              </span>
+              <Popconfirm
+                title={`${geree?.talbainDugaar} талбайн мөр бичилт устгах уу?`}
+                okText="Тийм"
+                cancelText="Үгүй"
+                onConfirm={() =>
+                  setGereenuud((a) => {
+                    a.splice(index, 1);
+                    return [...a];
+                  })
+                }
+              >
+                <span className="h-10 w-10 p-1 text-red-500">
+                  <CloseCircleOutlined />
+                </span>
+              </Popconfirm>
+            </div>
+            {geree?.baritsaaniiUldegdel > 0 && (
+              <div className="grid w-full grid-cols-4 rounded-md border border-gray-400 bg-gray-100 p-1">
+                <div className="col-span-4">Барьцаа дүн</div>
+                <div>{formatNumber(geree.baritsaaniiUldegdel)}</div>
+                <div>{geree.talbainDugaar}</div>
+                <div className="text-center">
+                  {moment(geree.gereeniiOgnoo).format("YYYY-MM-DD")}
+                </div>
+                <div className="text-right text-indigo-600">
+                  <input
+                    className="w-full rounded-md border bg-gray-200 px-2 text-right"
+                    placeholder="Барьцаа дүн"
+                    value={formatter(geree.baritsaaTulbur)}
+                    onDoubleClick={({ target }) =>
+                      onDoubleClickKholbokhDun(target, index, "baritsaaTulbur")
+                    }
+                    onChange={({ target }) => {
+                      onChangeKholbokhDun(target, index, "baritsaaTulbur");
+                    }}
+                  />
+                </div>
+              </div>
+            )}
+            {geree && (
+              <div className="grid w-full grid-cols-4 rounded-md border border-gray-400 bg-gray-100 p-1">
+                <div className="col-span-4">Түрээсийн төлбөр</div>
+                <div>{formatNumber(geree.uldegdel)}</div>
+                <div>{geree.talbainDugaar}</div>
+                <div className="text-center">
+                  {moment(geree.gereeniiOgnoo).format("YYYY-MM-DD")}
+                </div>
+                <div className="text-right text-green-600">
+                  <input
+                    className="w-full rounded-md border bg-gray-200 px-2 text-right"
+                    placeholder="Төлөх дүн"
+                    value={formatter(geree.tureesiinTulbur)}
+                    onDoubleClick={({ target }) =>
+                      onDoubleClickKholbokhDun(target, index, "tureesiinTulbur")
+                    }
+                    onChange={({ target }) => {
+                      onChangeKholbokhDun(target, index, "tureesiinTulbur");
+                    }}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <Divider />
+      <div className="grid w-full grid-cols-4 px-2">
+        <div className="col-span-2">
+          Гүйлгээний огноо:{moment().format("YYYY-MM-DD")}
+        </div>
+        <div className="text-center">Зөрүү дүн:</div>
+        <div className="text-right text-xl text-red-600">
+          {formatNumber(zuruuDun)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default React.forwardRef(GuilgeeNiiluulekh);
