2e4da3c248f6640dc4ded047cd132c04df69c5b3
baterdenets <bbbatuk99@gmail.com>
2022-09-27 09:37:22 +0800

    zasvar

--- next.config.js
+++ next.config.js
@@ -2,4 +2,15 @@ module.exports = {
   experimental: {
     outputStandalone: true,
   },
+  env: {
+    customKey: "my-value",
+  },
+  serverRuntimeConfig: {
+    HTTP_URL: process.env.HTTP_URL || "http://103.143.40.230:8081", // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    URL: process.env.URL || "https://turees.zevtabs.mn/api",
+    SOCKET: process.env.SOCKET || "https://turees.zevtabs.mn",
+  },
 };
--- tools/function/erkhteiEsekh.js
+++ tools/function/erkhteiEsekh.js
@@ -1,7 +1,14 @@
 import uilchilgee from "services/uilchilgee";
+import getConfig from "next/config";
+const { serverRuntimeConfig } = getConfig();
 function erkhteiEsekh(token, zam) {
   return uilchilgee(token)
-    .post("http://103.143.40.230:8081/erkhteiEsekh", { zam })
+    .post(
+      `${
+        serverRuntimeConfig.HTTP_URL || "http://103.143.40.230:8081"
+      }/erkhteiEsekh`,
+      { zam }
+    )
     .then(({ data }) => data);
 }
 
