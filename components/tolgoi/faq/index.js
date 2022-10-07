7e17b34e2aa1a729429ad5b82ee44296ecfa5ecc
Sodhuu <csodhuu@gmailcom>
2022-10-07 11:37:17 +0800

    faq bold solilt

--- components/tolgoi/faq/index.js
+++ components/tolgoi/faq/index.js
@@ -15,14 +15,14 @@ function tugeemelAsuult() {
           defaultActiveKey={["0"]}
           expandIcon={({ isActive }) => (
             <PlusOutlined
-              className="pt-1 pr-4 text-green-600"
+              className="pt-1 pr-4 text-green-600 "
               rotate={isActive ? 45 : 0}
             />
           )}
           ghost
         >
-          <Panel header={mur?.asuult} key="1">
-            <div className="px-16 pb-2 font-medium dark:text-white">
+          <Panel header={mur?.asuult} className="font-semibold" key="1">
+            <div className="px-16 pb-2 font-normal dark:text-white">
               {mur?.khariult}
             </div>
           </Panel>
