9f40d203a2fc88246fd1cc4efab7f35fee2dc585
baterdenets <bbbatuk99@gmail.com>
2022-10-12 11:40:12 +0800

    zasvar

--- components/pageComponents/tokhirgoo/AshiglaltiinZardal.js
+++ components/pageComponents/tokhirgoo/AshiglaltiinZardal.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useRef } from "react";
-import { Button, InputNumber, Popconfirm, Tooltip } from "antd";
+import { Button, Popconfirm, Tooltip } from "antd";
 import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
 import useJagsaalt from "hooks/useJagsaalt";
 import { useAuth } from "services/auth";
@@ -10,8 +10,10 @@ import deleteMethod from "tools/function/crud/deleteMethod";
 
 function AshiglaltiinZardal({ baiguullaga, token }) {
   const { barilgiinId } = useAuth();
-  const query = useMemo(() => ({ barilgiinId }), [barilgiinId]);
-  const ashiglaltiinZardal = useJagsaalt("/ashiglaltiinZardluud", query);
+  const khuvisakhQuery = useMemo(() => ({ barilgiinId }), [barilgiinId]);
+  const togtmolQuery = useMemo(() => ({ barilgiinId,togtmolEsekh:true }), [barilgiinId]);
+  const khuvisakhZardal = useJagsaalt("/ashiglaltiinZardluud", khuvisakhQuery);
+  const togtmolZardal = useJagsaalt("/ashiglaltiinZardluud", togtmolQuery);
   const ref = useRef();
 
   function zardalBurtgeye(data) {
@@ -31,7 +33,7 @@ function AshiglaltiinZardal({ baiguullaga, token }) {
           token={token}
           barilgiinId={barilgiinId}
           baiguullagiinId={baiguullaga?._id}
-          refresh={ashiglaltiinZardal.refresh}
+          refresh={khuvisakhZardal.refresh}
         />
       ),
       footer,
@@ -46,11 +48,11 @@ function AshiglaltiinZardal({ baiguullaga, token }) {
 
   return (
     <>
-      <div className="xxl:col-span-4 col-span-12 mt-5 lg:col-span-6">
+      <div className="xxl:col-span-4 col-span-12 mt-5 lg:col-span-5">
         <div className="box mt-5 lg:mt-0">
           <div className="dark:border-dark-5 flex items-center border-b border-gray-200 px-5 pt-5 pb-2">
             <h2 className="mr-auto text-base font-medium dark:text-gray-200">
-              Ашиглалтын зардал
+              Хувьсах зардал
             </h2>
             <div
               className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-green-500 fill-current p-2 text-white"
@@ -61,7 +63,59 @@ function AshiglaltiinZardal({ baiguullaga, token }) {
               </Tooltip>
             </div>
           </div>
-          {ashiglaltiinZardal.jagsaalt?.map((mur) => {
+          {khuvisakhZardal.jagsaalt?.map((mur) => {
+            return (
+              <div className="box" key={mur._id}>
+                <div className="flex items-center p-5">
+                  <div className="border-l-2 border-green-500 pl-4">
+                    <div className="font-medium">{mur.ner}</div>
+                    <div className="text-gray-600">{mur.turul}</div>
+                  </div>
+                  <div className="ml-auto">{formatNumber(mur.tariff)}</div>
+                  <div className="ml-5 flex space-x-2">
+                    <Popconfirm
+                      title={`${mur.ner} данс устгах уу?`}
+                      okText="Тийм"
+                      cancelText="Үгүй"
+                      onConfirm={() => ustgaya(mur)}
+                    >
+                      <div className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-red-500 fill-current p-2 text-white">
+                        <Tooltip title="Устгах">
+                          <DeleteOutlined size={20} />
+                        </Tooltip>
+                      </div>
+                    </Popconfirm>
+                    <div
+                      className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-yellow-500 fill-current p-2 text-white"
+                      onClick={() => zardalBurtgeye(mur)}
+                    >
+                      <Tooltip title="Засах">
+                        <EditOutlined />
+                      </Tooltip>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="xxl:col-span-4 col-span-12 mt-5 lg:col-span-4">
+        <div className="box mt-5 lg:mt-0">
+          <div className="dark:border-dark-5 flex items-center border-b border-gray-200 px-5 pt-5 pb-2">
+            <h2 className="mr-auto text-base font-medium dark:text-gray-200">
+              Тогтмол зардал
+            </h2>
+            <div
+              className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-green-500 fill-current p-2 text-white"
+              onClick={() => zardalBurtgeye()}
+            >
+              <Tooltip title="Нэмэх">
+                <PlusOutlined />
+              </Tooltip>
+            </div>
+          </div>
+          {togtmolZardal.jagsaalt?.map((mur) => {
             return (
               <div className="box" key={mur._id}>
                 <div className="flex items-center p-5">
