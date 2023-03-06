73503329d21db54214959c4509ce7f27e4dd320c
LBayandalai <bayanaa.work.tabs@gmail.com>
2023-03-06 12:05:46 +0800

    zasvar

--- tools/logic/khereglegchiinErkhiinTokhirgoo.js
+++ tools/logic/khereglegchiinErkhiinTokhirgoo.js
@@ -794,45 +794,26 @@ const khuudasnuud = [
       </svg>
     ),
   },
-  // {
-  //   ner: "Тоглоомын төв",
-  //   khuudasniiNer: "",
-  //   href: "/khyanalt/togloom",
-  //   icon: (
-  //     <svg
-  //       xmlns="http://www.w3.org/2000/svg"
-  //       width="24"
-  //       height="24"
-  //       viewBox="0 0 24 24"
-  //       fill="none"
-  //       stroke="currentColor"
-  //       stroke-width="2"
-  //       stroke-linecap="round"
-  //       stroke-linejoin="round"
-  //       icon-name="box"
-  //       data-lucide="box"
-  //       class="lucide lucide-box mx-auto block"
-  //     >
-  //       <path d="M21 16V8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z"></path>
-  //       <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
-  //       <line x1="12" y1="22.08" x2="12" y2="12"></line>
-  //     </svg>
-  //   ),
-  //   sub: [
-  //     // {
-  //     //   ner: "Жагсаалт",
-  //     //   khuudasniiNer: "togloom2",
-  //     //   href: "/khyanalt/togloom/togloom2",
-  //     //   icon: <FaSteam style={{ width: "24", height: "24" }} />,
-  //     // },
-  //     {
-  //       ner: "Тоглоомын төв",
-  //       khuudasniiNer: "togloomTuv",
-  //       href: "/khyanalt/togloom/togloomTuv",
-  //       icon: <TbLego style={{ width: "24", height: "24" }} />,
-  //     },
-  //   ],
-  // },
+  {
+      ner: "Тоглоомын төв",
+      khuudasniiNer: "togloomTuv",
+      href: "/khyanalt/togloom/togloomTuv",
+      icon: <TbLego style={{ width: "24", height: "24" }} />,
+    // sub: [
+    //   {
+    //     ner: "Жагсаалт",
+    //     khuudasniiNer: "togloom2",
+    //     href: "/khyanalt/togloom/togloom2",
+    //     icon: <FaSteam style={{ width: "24", height: "24" }} />,
+    //   },
+    //   {
+    //     ner: "Тоглоомын төв",
+    //     khuudasniiNer: "togloomTuv",
+    //     href: "/khyanalt/togloom/togloomTuv",
+    //     icon: <TbLego style={{ width: "24", height: "24" }} />,
+    //   },
+    // ],
+  },
 ];
 
 function useErkh(ajiltan) {
