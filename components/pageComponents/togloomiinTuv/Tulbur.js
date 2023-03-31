e739ce5770eb1921d1d98b377c4a9a15bba2feae
LBayandalai <bayanaa.work.tabs@gmail.com>
2023-03-31 17:45:42 +0800

    zasvar

--- components/pageComponents/togloomiinTuv/Tulbur.js
+++ components/pageComponents/togloomiinTuv/Tulbur.js
@@ -6,16 +6,19 @@ import axios from "axios";
 import moment from "moment";
 import KhuvaajTulukh from "./KhuvaajTulukh";
 import EBarimt from "./EBarimt";
+import QRCode from "react-qr-code";
 import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import uilchilgee, { aldaaBarigch } from "services/uilchilgee";
 import { useEffect } from "react";
 import { t } from "i18next";
+import { useQRCode } from "next-qrcode";
 //#endregion
 
 function Tulbur(
   { destroy, data, token, ajiltan, baiguullaga, barilgiinId, onRefresh },
   ref
 ) {
+  const { Canvas } = useQRCode();
   const [alkham, setAlkham] = React.useState(
     data?.tulburTulsunEsekh === true ? 2 : 1
   );
@@ -69,13 +72,18 @@ function Tulbur(
         .then(({ data }) => {
           if (data.success === true) {
             setEBarimt(data);
-            handlePrint();
             onRefresh();
           }
         })
         .catch(aldaaBarigch);
     }
   }
+
+  useEffect(()=> {
+    if (!!eBarimt) {      
+      handlePrint();
+    }
+  },[eBarimt])
 
   function batalgaajuulya(turul, val) {    
     if (turul === "khaan") {
@@ -180,6 +188,243 @@ function Tulbur(
 
   return (
     <div className="w-full h-full">
+      {eBarimt && (
+          <div className="hidden">
+            <div className="p-2" style={{ minWidth: "20rem" }} ref={eBarimtRef}>
+              <table className="w-full">
+                <colgroup>
+                  <col className="w-1/6" />
+                  <col className="w-1/6" />
+                  <col className="w-1/6" />
+                  <col className="w-1/6" />
+                  <col className="w-1/6" />
+                  <col className="w-1/6" />
+                </colgroup>
+                <tbody className="text-xs">
+                  <tr>
+                    <td colSpan={6} className="text-center border">
+                      {`${baiguullagaEsekh ? "ААН-д" : "Иргэнд"} очих баримт`}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={6} className="text-center border">
+                      {baiguullaga?.ner}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={6} className="font-medium border">
+                      {t("Борлуулагч")}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border" colSpan={3}>
+                      {t("Огноо")}
+                    </td>
+                    <td className="border" colSpan={3}>
+                      {moment(eBarimt?.date).format("YYYY/MM/DD hh:mm:ss")}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border" colSpan={3}>
+                      {t("ТТД")}
+                    </td>
+                    <td className="border" colSpan={3}>
+                      {eBarimt?.registerNo}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border" colSpan={3}>
+                      {t("ДДТД")}
+                    </td>
+                    <td className="border" colSpan={3}>
+                      {eBarimt?.billId}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border" colSpan={3}>
+                      {t("Касс")}
+                    </td>
+                    <td className="border" colSpan={3}>
+                      {eBarimt?.posNo}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border" colSpan={3}>
+                      {t("Кассчин")}
+                    </td>
+                    <td className="border" colSpan={3}>
+                      {ajiltan?.ner}
+                    </td>
+                  </tr>
+                  {baiguullagaEsekh && (
+                    <>
+                      <tr>
+                        <td className="border" colSpan={6}>
+                          {t("Худалдан авагч")}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="border" colSpan={1}>
+                          {t("ТТД")}
+                        </td>
+                        <td className="border" colSpan={5}>
+                          {register}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="border" colSpan={1}>
+                          {t("Нэр")}
+                        </td>
+                        <td className="border" colSpan={5}>
+                          {baiguullagiinMedeelel?.name}
+                        </td>
+                      </tr>
+                    </>
+                  )}
+                  <tr>
+                    <td colSpan={6} className="border">
+                      <br />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border text-center" colSpan={3}>
+                      {t("Барааны нэр")}
+                    </td>
+                    <td className="border text-center">{t("Тоо")}</td>
+                    <td className="border text-center">{t("Нэгж")}</td>
+                    <td className="border text-center">{t("Дүн")}</td>
+                  </tr>
+                  {eBarimt?.stocks?.map((mur, index) => (
+                    <tr key={`${index}-zakhialga`}>
+                      <td colSpan={3} className="border text-right">
+                        {mur.name}
+                      </td>
+                      <td className="border text-right">{mur.qty}</td>
+                      <td className="border text-right">
+                        {formatNumber(mur.unitPrice, 2)}
+                      </td>
+                      <td className="border text-right">
+                        {formatNumber(mur.totalAmount, 2)}
+                      </td>
+                    </tr>
+                  ))}
+                  <tr>
+                    <td colSpan={5} className="text-right border">
+                      {t("НӨАТ-гүй дүн")}
+                    </td>
+                    <td className="border text-right">
+                      {formatNumber(data.niitDun / 1.1, 2)}
+                    </td>
+                  </tr>
+                  {data?.khungulukhKhuvi && (
+                    <tr>
+                      <td colSpan={5} className="text-right border">
+                        {t("Хөнгөлөлт")}
+                      </td>
+                      <td className="border text-right">
+                        {formatNumber(data.niitUndsenDun - data.niitDun)}
+                      </td>
+                    </tr>
+                  )}
+                  <tr>
+                    <td colSpan={5} className="text-right border">
+                      {t("НӨАТ-н дүн")}
+                    </td>
+                    <td className="border text-right">
+                      {formatNumber(eBarimt?.vat, 2)}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={5} className="text-right border">
+                      {t("Төлөх дүн")}
+                    </td>
+                    <td className="border text-right">
+                      {formatNumber(eBarimt?.amount)}
+                    </td>
+                  </tr>
+                  {tulbur?.belen && (
+                    <tr>
+                      <td colSpan={5} className="text-right border">
+                        {t("Бэлнээр")}
+                      </td>
+                      <td className="border text-right">
+                        {formatNumber(tulbur?.belen)}
+                      </td>
+                    </tr>
+                  )}
+                  {tulbur?.belenBus && (
+                    <tr>
+                      <td colSpan={5} className="text-right border">
+                        {t("Бэлэн бусаар")}
+                      </td>
+                      <td className="border text-right">
+                        {formatNumber(tulbur?.belenBus)}
+                      </td>
+                    </tr>
+                  )}
+                  {tulbur?.khariult && (
+                    <tr>
+                      <td colSpan={5} className="text-right border">
+                        {t("Хариулт")}
+                      </td>
+                      <td className="border text-right">
+                        {formatNumber(tulbur?.khariult)}
+                      </td>
+                    </tr>
+                  )}
+                  <tr>
+                    <td colSpan={4} className="border">{`Е-Баримт ${baiguullagaEsekh ? "" : "уншуулах"
+                      } дүн`}</td>
+                    <td colSpan={2} className="border text-right">
+                      {formatNumber(eBarimt?.amount)}
+                    </td>
+                  </tr>
+                  {!baiguullagaEsekh && (
+                    <tr>
+                      <td colSpan={4} className="border">
+                        {t("Сугалааны дугаар")}
+                      </td>
+                      <td colSpan={2} className="border">
+                        {eBarimt?.lottery}
+                      </td>
+                    </tr>
+                  )}
+                  <tr>
+                    <td colSpan={6}>
+                      <div className="w-full flex justify-center p-5">
+                        <div className="w-40 h-40">
+                          <QRCode value={eBarimt?.qrData} size={160} />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={6}>
+                      <div className="w-full border-dashed border-t-2 pt-5 mt-3 border-black h-full flex flex-col justify-center items-center">
+                        <div className=" text-justify text-xs"><div>Эхлэх хугацаа: {moment(data?.ekhlekhTsag).format("YYYY-MM-DD HH:mm")}</div> <div>Дуусах хугацаа: {moment(data?.duusakhTsag).format("YYYY-MM-DD HH:mm")}</div></div>
+                        <div className="w-full justify-center flex items-center"><Canvas
+                          text={moment(data?.duusakhTsag).format("YYYYMMDDHHmmss")}
+                          options={{
+                            level: 'M',
+                            margin: 3,
+                            scale: 4,
+                            width: 200,
+                            color: {
+                              dark: '#000000',
+                              light: '#FFFFFF',
+                            },
+                          }}
+                        />
+                        </div>
+                        <div className="text-center text-xs max-w-[400px]">Энэхүү QR код нь тоглох хүчинтэй хугацаанд зөвхөн нэг удаа нэвтэрч ороход ашиглагдахыг анхаарна уу!</div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
       <div className="pb-2">
         <Steps className="hidden" current={alkham}>
           <Steps.Step
