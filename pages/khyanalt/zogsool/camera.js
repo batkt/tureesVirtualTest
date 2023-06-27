b5db4466393a5b00fbc1a090b6419561cba52915
Hosoo <a_xocoo@yahoo.com>
2023-06-27 15:36:40 +0800

    parking

--- pages/khyanalt/zogsool/camera.js
+++ pages/khyanalt/zogsool/camera.js
@@ -972,7 +972,7 @@ function camera({token}) {
                                     onChange={setOgnoo}
                                 />
                                 <div className="ml-5 flex space-x-2 p-1 pt-2 text-base font-medium">
-                                    {t("Бүртгэгдсэн орлого")} :{" "}
+                                    {t("Нийт бодогдсон")} :{" "}
                                     {
                                         formatNumber(
                                             !!orlogo[0]?.niitDun ? orlogo[0].niitDun : 0, 0
@@ -981,7 +981,7 @@ function camera({token}) {
                                     ₮
                                 </div>
                                 <div className="ml-5 flex space-x-2 p-1 pt-2 text-base font-medium">
-                                    {t("Зогсоолын орлого")} :{" "}
+                                    {t("Нийт төлсөн")} :{" "}
                                     {
                                         formatNumber(
                                             !!orlogo[0]?.dun ? orlogo[0].dun : 0, 0
