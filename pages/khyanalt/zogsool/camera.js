80ece87ca558769e023ad7cc05d43b417baff873
Bat-Erdene <123363316+hanargaghd@users.noreply.github.com>
2023-07-05 14:41:20 +0800

    excelDunShaltgaanZasvar

--- pages/khyanalt/zogsool/camera.js
+++ pages/khyanalt/zogsool/camera.js
@@ -1139,10 +1139,7 @@ function camera({ token }) {
                 </div>
                 <div className="ml-5 flex space-x-2 p-1 pt-2 text-base font-medium">
                   {t("Төлсөн")} :{" "}
-                  {formatNumber(
-                      !!orlogo[0]?.dun ? orlogo[0].dun : 0, 0
-                  )}
-                  ₮
+                  {formatNumber(!!orlogo[0]?.dun ? orlogo[0].dun : 0, 0)}₮
                 </div>
               </div>
               <div
@@ -1240,20 +1237,18 @@ function camera({ token }) {
                                     title: "Дүн",
                                     dataIndex: "tuukh",
                                     render(v, p, i) {
-                                      return formatNumber(v[i]?.tulukhDun, 0);
+                                      return v?.[0]?.tulukhDun || 0;
                                     },
                                   },
                                   {
-                                    title: t("Шалтгаан"),
-                                    align: "center",
+                                    title: "Шалтгаан",
                                     dataIndex: "tuukh",
-                                    width: "7rem",
-                                    showSorterTooltip: false,
                                     render: (v, parent) => {
-                                      return v[0]?.tuluv === -1
-                                        ? v[0]?.uneguiGarsan
-                                        : !!v[0]?.tsagiinTuukh[0]?.garsanTsag &&
-                                          v[0]?.niitKhugatsaa <= 30
+                                      return v?.[0]?.tuluv === -1
+                                        ? v?.[0]?.uneguiGarsan
+                                        : !!v?.[0]?.tsagiinTuukh[0]
+                                            ?.garsanTsag &&
+                                          v?.[0]?.niitKhugatsaa <= 30
                                         ? "30 мин"
                                         : parent.zurchil;
                                     },
