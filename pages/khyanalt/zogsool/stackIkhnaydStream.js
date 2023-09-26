774a00208046566833c5a5523c342a1c548fca9c
Bat-Erdene <123363316+hanargaghd@users.noreply.github.com>
2023-09-26 20:39:10 +0800

    zasvar

--- pages/khyanalt/zogsool/stackIkhnaydStream.js
+++ pages/khyanalt/zogsool/stackIkhnaydStream.js
@@ -17,12 +17,12 @@ function StackIkhNaydStream({ barilgiinId }) {
   console.log("cameraIps", cameraIps);
 
   return (
-    <div className="grid xl:grid-cols-1 2xl:grid-cols-2">
+    <div className="grid md:grid-cols-2">
       {cameraIps?.map((mur, index) => {
         console.log("mur", mur);
         return (
           <div key={index} className="border">
-            <div className="h-[400px] w-[600px]">
+            <div className="h-[200px] w-[300px] sm:h-[300px] sm:w-[400px] xl:h-[400px] xl:w-[600px]">
               <R2WPlayerComponent
                 Camer={mur}
                 PASSWD={"123456"}
