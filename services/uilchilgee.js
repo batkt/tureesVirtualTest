a9115d3160927e51267d93fd3d75dcc7db3c02ab
Tsend16 <d.is11d102@gmail.com>
2023-10-26 17:32:11 +0800

    test xiij shalgaw

--- services/uilchilgee.js
+++ services/uilchilgee.js
@@ -7,7 +7,7 @@ import { t } from "i18next";
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 export const url =
-  // "http://192.168.1.13:8081";
+  //  "http://192.168.1.13:8081";
   publicRuntimeConfig.URL || "https://turees.zevtabs.mn/api";
 
 export const socket = () =>
