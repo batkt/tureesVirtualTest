a35c909b8c01c313cb0c75b9e842551550c6d941
Tsend16 <d.is11d102@gmail.com>
2023-10-26 18:32:31 +0800

    req

--- pages/api/update.js
+++ pages/api/update.js
@@ -1,7 +1,7 @@
 const execSync = require("child_process").execSync;
 
 export default function handler(req, res) {
-  const githubEvent = request.headers["x-github-event"];
+  const githubEvent = req.headers["x-github-event"];
   if (githubEvent === "push")
     var output = execSync("yarn update", { encoding: "utf-8" });
   res.status(202).send("Accepted");
