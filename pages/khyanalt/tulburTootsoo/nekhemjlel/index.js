c0f5c88b9ad44b2d3f693f07b01af57c7dfa43df
Bat-Erdene <123363316+hanargaghd@users.noreply.github.com>
2023-11-16 18:22:41 +0800

    khemjeeZasvar

--- pages/khyanalt/tulburTootsoo/nekhemjlel/index.js
+++ pages/khyanalt/tulburTootsoo/nekhemjlel/index.js
@@ -1114,7 +1114,7 @@ function tulburTootsoo({ token }) {
                   className={
                     !nekhemjlekh.khatuuZagvarEsekh
                       ? `print ${nekhemjlekh.khuudasniiKhemjee}-${nekhemjlekh.chiglel} sun-editor-editable p-10"`
-                      : "A4-portrait hidden overflow-hidden text-xs print:block"
+                      : "hidden overflow-hidden text-xs print:block"
                   }
                   dangerouslySetInnerHTML={{
                     __html: nekhemjlekh.zagvar,
