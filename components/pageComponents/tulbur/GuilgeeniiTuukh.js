e6c953e0587a54f8daf16c0f3108bec6ba7cb81f
Mungunshagai753 <89889019+Mungunshagai753@users.noreply.github.com>
2024-03-22 16:38:42 +0800

    zasvar
    zasvar

--- components/pageComponents/tulbur/GuilgeeniiTuukh.js
+++ components/pageComponents/tulbur/GuilgeeniiTuukh.js
@@ -348,68 +348,57 @@ function GuilgeeniiTuukh(
           <tr className="flex min-w-[93rem] divide-x divide-white border-b border-gray-200 bg-gray-200 pr-1 text-gray-700  dark:bg-gray-800 dark:text-gray-400">
             <td
               onClick={() => toggleSortOrder("ognoo")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Огноо")}
             </td>
             <td
               onClick={() => toggleSortOrder("turees")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Түрээс")}
             </td>
             <td
               onClick={() => toggleSortOrder("tulukhDun")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Төлөх дүн")}
             </td>
             <td
               onClick={() => toggleSortOrder("khyamdral")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Хямдрал")}
             </td>
             <td
               onClick={() => toggleSortOrder("tulsunAldangi")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Төлсөн алданги")}
             </td>
             <td
               onClick={() => toggleSortOrder("tulsunDun")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Төлсөн дүн")}
             </td>
             <td
               onClick={() => toggleSortOrder("uldegdel")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Үлдэгдэл")}
             </td>
             <td
               onClick={() => toggleSortOrder("ajiltan")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Ажилтан")}
             </td>
             <td
               onClick={() => toggleSortOrder("helber")}
-              className="min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Хэлбэр")}
             </td>
             <td
               onClick={() => toggleSortOrder("tailbar")}
-              className="w-full min-w-[8rem] overflow-hidden p-1 text-center"
-            >
+              className="w-full min-w-[8rem] overflow-hidden p-1 text-center">
               {t("Тайлбар")}
             </td>
             <td
               onClick={() => toggleSortOrder("burtgesenOgnoo")}
-              className="min-w-[10rem] p-1 text-center"
-            >
+              className="min-w-[10rem] p-1 text-center">
               {t("Бүртгэсэн огноо")}
             </td>
             <td className="min-w-[3rem] border-none p-1 text-center"></td>
@@ -417,8 +406,7 @@ function GuilgeeniiTuukh(
         </th>
         <tbody
           className=" overflownone min-w-[93.4rem] overflow-y-scroll"
-          style={{ height: "calc(100vh - 15rem)" }}
-        >
+          style={{ height: "calc(100vh - 15rem)" }}>
           {sortedData
             ?.map((a, i) => (
               <tr className="flex min-w-[93rem] divide-x border-b border-gray-200 bg-gray-50 text-gray-700 hover:bg-green-100 dark:bg-gray-700 dark:text-gray-400">
@@ -443,8 +431,7 @@ function GuilgeeniiTuukh(
                 <td
                   className={`min-w-[8rem] overflow-hidden p-1 text-end ${
                     a?.uldegdel > 0 ? "text-red-500" : "text-green-500"
-                  }`}
-                >
+                  }`}>
                   {formatNumber(
                     a.turul === "khyamdral" && a.uldegdel < 0 ? 0 : a.uldegdel,
                     0
@@ -481,13 +468,13 @@ function GuilgeeniiTuukh(
                       a.turul === "aldangi" ||
                       a.turul === "zalruulga" ||
                       a.turul === "baritsaa" ||
-                      a.turul === "qpay") && (
+                      a.turul === "qpay" ||
+                      a.turul === "tulultBurtgekh") && (
                       <Popconfirm
                         title={t("Төлөлт устгах уу?")}
                         okText={t("Тийм")}
                         cancelText={t("Үгүй")}
-                        onConfirm={() => tulultUstgaya(a)}
-                      >
+                        onConfirm={() => tulultUstgaya(a)}>
                         <div className="hide-on-print flex h-6 w-6 cursor-pointer items-center justify-center rounded-full border p-1 text-red-500">
                           <DeleteOutlined />
                         </div>
