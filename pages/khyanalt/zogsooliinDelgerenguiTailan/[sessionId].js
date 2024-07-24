b1ca914d2807cf6532be04ab01dac16340cccfa0
Tsend16 <d.is11d102@gmail.com>
2024-07-24 17:23:18 +0800

    zasaw

--- pages/khyanalt/zogsooliinDelgerenguiTailan/[sessionId].js
+++ pages/khyanalt/zogsooliinDelgerenguiTailan/[sessionId].js
@@ -283,6 +283,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Дансаар",
                 icon: "https://static.vecteezy.com/system/resources/previews/012/487/823/original/3d-hand-press-pay-button-icon-phone-with-credit-card-float-on-transparent-mobile-banking-online-payment-service-withdraw-money-easy-shop-cashless-society-concept-cartoon-minimal-3d-render-png.png",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -291,6 +292,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Бэлэн",
                 icon: "https://static.vecteezy.com/system/resources/previews/012/958/770/original/payment-icon-for-shopping-online-3d-hand-holding-banknote-cartoon-businessman-wearing-suit-holds-money-floating-isolated-on-transparent-withdraw-money-easy-shopping-concept-3d-minimal-rendering-png.png",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -299,6 +301,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Хөнгөлөх",
                 icon: "https://static.vecteezy.com/system/resources/previews/012/487/845/original/3d-wallet-floating-in-hand-isolated-on-transparent-business-man-holding-purple-purse-icon-mobile-banking-online-service-cashback-refund-loan-concept-saving-money-wealth-cartoon-3d-render-png.png",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -307,6 +310,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Хаан банк",
                 icon: "https://play-lh.googleusercontent.com/Aw4bwCDJgAzu6AFAbbcfCFpheVMB6ZKiEM3JlrJ3cAM65fK-1QaTZZs_Vk4UFBzykQ=s480-rw",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -315,6 +319,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "TDB банк",
                 icon: "https://tz.mn/storage/uploads/slider/45adc5a14070aa.jpg",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -323,6 +328,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Xac банк",
                 icon: "https://cdn6.aptoide.com/imgs/0/6/d/06df97a06fbc7622a775a7c414b69e87_icon.png",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -331,6 +337,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Голомт банк",
                 icon: "https://play-lh.googleusercontent.com/9tUBesUsI4UIkpgO1MPIMLFvhDa_4vZE75TrVAUHFA7a0bJ7IIgeyh2r1QXs9VlmXmkX",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -339,6 +346,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Капитрон банк",
                 icon: "https://play-lh.googleusercontent.com/1PMmu0x2x_07XdPtLyTRe_4cffXDLFCG3xEoUTqUpy3eSJeB-C81dbyzZSnJjW907OA=w240-h480-rw",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -347,6 +355,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: "Төрийн банк",
                 icon: "https://play-lh.googleusercontent.com/KYQyVTgP4ZV60gxNOsKYssScNe17NMgHpO_nRY4WRBYj_4YTZ0e8t6zwh38sTFmyCco",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -355,6 +364,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: element._id,
                 icon: "https://qpay.mn/q/img/q.png",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -363,6 +373,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: element._id,
                 icon: "https://play-lh.googleusercontent.com/GofyFzRM2Kwf3d47fl6FibZB7kE16Aljaodzc-ghiJmdiPpGljaqeop2T6JaURd8rw=s480-rw",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -371,6 +382,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: element._id,
                 icon: "https://play-lh.googleusercontent.com/Jg_jjsNezlkTuxWT5ADzfqhjwHVvqZEDqQGbXJlkplNrYPyyMGXtmLA6dGrH37_paOY=w240-h480-rw",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -387,6 +399,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: element._id,
                 icon: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.toki.mn%2F&psig=AOvVaw1ej7t5Vo6qLIhInQ0XNMEm&ust=1715243807766000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjD-6DT_YUDFQAAAAAdAAAAABAE",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
@@ -396,6 +409,7 @@ function TulburiinDelgerenguiTailan({ token }) {
                 ner: element._id,
                 icon: "https://static.vecteezy.com/system/resources/previews/012/958/770/original/payment-icon-for-shopping-online-3d-hand-holding-banknote-cartoon-businessman-wearing-suit-holds-money-floating-isolated-on-transparent-withdraw-money-easy-shopping-concept-3d-minimal-rendering-png.png",
                 dun: element.niitDun,
+                too: element.niitToo,
                 khuvi: (Number(element.niitDun) / Number(niitDun)) * 100,
               });
               break;
