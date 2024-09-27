ab10e02603c52fd069fe518ce83ba1baa09aa4fe
Tsend16 <d.is11d102@gmail.com>
2024-09-27 14:38:10 +0800

    xooson push

--- components/pageComponents/eBarimt/Tulbur.js
+++ components/pageComponents/eBarimt/Tulbur.js
@@ -28,6 +28,7 @@ function Tulbur(
   const [baiguullagaEsekh, setBaiguullagaEsekh] = React.useState(
     defaultTurul === "ААН" ? true : false
   );
+
   const [irgenEsekh, setIrgenEsekh] = React.useState(
     defaultTurul === "Иргэн" ? true : false
   );
