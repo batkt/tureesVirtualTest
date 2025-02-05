49baffe996d967b9af3945ba2a96b682bcdd9de8
tsend16 <d.is11d102@gmail.com>
2025-02-05 09:59:09 +0800

    z

--- tools/zagvar/turGotoMPM.js
+++ tools/zagvar/turGotoMPM.js
@@ -248,7 +248,7 @@ const khatuuZagvarGotoMPM = (medeelel, ajiltan, baiguullaga, barilgiinId) => {
     <div style="margin-top: 1rem; display: flex; width: 100%; align-items: flex-start; justify-content: space-between;">
       <div style="width: 40%;">
         <div style="display: flex; align-items: flex-start; justify-content: space-between;">
-          <p style="white-space: nowrap;">Нягтлан бодогч: &nbsp;&nbsp;&nbsp;&nbsp;&lt;gariinUseg&gt;</p>
+          <p style="white-space: nowrap;">Нягтлан бодогч: &nbsp;&nbsp;&nbsp;&lt;gariinUseg&gt;</p>
           <p style="width: 40%; text-align: left; font-weight: 600;">
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; / Б.Хонгорзул/ 
           </p>  
