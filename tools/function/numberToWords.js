3e0fc6a66c892306caaf51ed6ef939c935deadc9
batkt <168392649+batkt@users.noreply.github.com>
2025-03-25 13:41:46 +0800

    number

--- tools/function/numberToWords.js
+++ tools/function/numberToWords.js
@@ -3,8 +3,8 @@ import { toWords } from "mon_num";
 
 function numberToWords(number, otion, bukhel, butarhai) {
   const { fixed, suffix } = otion;
-  let resValue = "";
-  const value = number?.toFixed(fixed)?.toString();
+  let resValue = number < 0 ? "хасах " : "";
+  const value = (number < 0 ? (number * (-1)) : number)?.toFixed(fixed)?.toString();
   if (value?.includes(".")) {
     resValue = toWords(Number(value.split(".")[0]), { suffix });
     if (!!bukhel) resValue += ` ${bukhel}`;
