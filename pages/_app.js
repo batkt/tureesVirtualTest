98e6050821692726f52fc6326cdfa5af45c6e940
buynaaaa24 <abuynaa1211@gmail.com>
2025-08-15 11:47:26 +0800

    OFFWEBFIX

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import { AuthProvider } from "../services/auth";
 import { ThemeProvider } from "next-themes";
+import { registerServiceWorker } from "@/utils/registerServiceWorker";
 import "antd/dist/antd.css";
 import "../styles/globals.css";
 import "suneditor/dist/css/suneditor.min.css";
@@ -7,6 +9,10 @@ import "aos/dist/aos.css";
 import "../services/i18n";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    registerServiceWorker(); // register SW only on client
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <AuthProvider>
