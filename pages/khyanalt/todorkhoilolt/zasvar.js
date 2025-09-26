f33a0d255197b47b1c535711fe3931273ace9252
Zola <zzolbaya@gmail.com>
2025-09-26 11:40:42 +0800

    zasla

--- pages/khyanalt/todorkhoilolt/zasvar.js
+++ pages/khyanalt/todorkhoilolt/zasvar.js
@@ -47,53 +47,28 @@ const talbainiiTalbaruud = [
   { ner: "Талбайн хэмжээ м2", talbar: "talbainKhemjee" },
   { ner: "Талбайн нийт үнэ үсгээр", talbar: "talbainNiitUneUsgeer" },
 ];
+
 const customPlugin = ({
   name = "custom_example",
   title = t("Талбарийн нэр"),
   button = "T",
-  songokhTalbaruud = talbaruud,
+  songokhTalbaruud = [],
 }) => ({
-  // @Required @Unique
-  name: name,
-  // @Required
-  display: "container" || "command" || "submenu" || "dialog",
-
-  // @options
-  // * You can also set from the button list
-  // HTML title attribute (tooltip) - default: plugin's name
-  title: title,
-  // HTML to be append to button (icon)
-  // Recommend using the inline svg icon. - default: "<span class="se-icon-text">!</span>"
-
+  name,
+  display: "container",
+  title,
   innerHTML: `<span style="padding:5px;">${button}</span>`,
-  // The class of the button. - default: "se-btn"
-  // "se-code-view-enabled": It is not disable when on code view mode.
-  // "se-resizing-enabled": It is not disable when on using resizing module.
   buttonClass: "",
-
-  // @Required
   add: function (core, targetElement) {
-    // Generate submenu HTML
-    // Always bind "core" when calling a plugin function
     let listDiv = this.setSubmenu.call(core);
-
-    // You must bind "core" object when registering an event.
-    /** add event listeners */
-    var self = this;
+    const self = this;
     listDiv.querySelectorAll(".se-btn-list").forEach(function (btn) {
       btn.addEventListener("click", self.onClick.bind(core));
     });
-
-    // @Required
-    // You must add the "submenu" element using the "core.initMenuTarget" method.
-    /** append target button menu */
     core.initMenuTarget(this.name, targetElement, listDiv);
   },
-
   setSubmenu: function () {
     const listDiv = this.util.createElement("DIV");
-    // @Required
-    // A "se-submenu" class is required for the top level element.
     listDiv.className = "se-submenu se-list-layer";
     listDiv.innerHTML =
       '<div class="se-list-inner se-list-font-size"><ul class="se-list-basic">' +
@@ -104,7 +79,6 @@ const customPlugin = ({
         )
         .join("") +
       "</ul></div>";
-
     return listDiv;
   },
   onClick: function (e) {
@@ -112,24 +86,25 @@ const customPlugin = ({
     const node = this.util.createElement("span");
     this.util.addClass(node, "se-custom-tag");
     node.textContent = value;
-
     this.insertNode(node);
     const zeroWidthSpace = this.util.createTextNode(this.util.zeroWidthSpace);
     node.parentNode.insertBefore(zeroWidthSpace, node.nextSibling);
     this.submenuOff();
   },
 });
+
 function Zasvar({ value, change, data, read }, ref) {
   const editorRef = React.useRef();
-  const plugins = React.useMemo(
-    () => require("suneditor/src/plugins")?.default || {},
-    []
-  );
-  console.log(data);
-  const initialValue = data?.mail;
-
-  const [utga, setUtga] = useState(initialValue);
+  const [plugins, setPlugins] = useState({});
+  const [utga, setUtga] = useState(data?.mail || "");
   const [editorReady, setEditorReady] = useState(false);
+
+  // ✅ load suneditor plugins only in browser
+  React.useEffect(() => {
+    import("suneditor/src/plugins").then((mod) => {
+      setPlugins(mod.default || {});
+    });
+  }, []);
 
   React.useImperativeHandle(ref, () => ({
     editor: editorRef.current?.editor,
@@ -151,38 +126,13 @@ function Zasvar({ value, change, data, read }, ref) {
     if (data) {
       const newValue = data.mail || "";
       setUtga(newValue);
-
       if (editorRef.current?.editor && editorReady) {
         editorRef.current.editor.setContents(newValue);
       }
     }
   }, [data, editorReady]);
 
-  // function garya() {
-  //   const currentValue = data?.mail;
-  //   if (utga !== currentValue)
-  //     Modal.confirm({
-  //       content: t("Та хадгалахгүй гарахдаа итгэлтэй байна уу?"),
-  //       okText: t("Тийм"),
-  //       cancelText: t("Үгүй"),
-  //       onOk: destroy(),
-  //     });
-  //   else destroy();
-  // }
-
-  // React.useEffect(() => {
-  //   function keyUp(e) {
-  //     if (e.key === "Escape") {
-  //       e.preventDefault();
-  //       garya();
-  //     }
-  //   }
-  //   document.addEventListener("keyup", keyUp);
-  //   return () => document.removeEventListener("keyup", keyUp);
-  // }, [utga]);
-
   const custom = React.useMemo(() => {
-    let songokhTalbaruud = [];
     const undsen = customPlugin({
       songokhTalbaruud: undsenTalbaruud,
       name: "undsen",
@@ -201,43 +151,30 @@ function Zasvar({ value, change, data, read }, ref) {
       title: t("Түрээсийн талбай"),
       button: renderToString(<BankOutlined />),
     });
-
     return [undsen, khugatsaa, talbai];
   }, []);
 
   const handleStringChange = React.useCallback(
     (content) => {
       setUtga(content);
-      if (change) {
-        change(content);
-      }
+      if (change) change(content);
     },
     [change]
   );
 
   const handleEditorLoad = React.useCallback(() => {
     setEditorReady(true);
-
-    if (editorRef.current?.editor && initialValue) {
+    if (editorRef.current?.editor && utga) {
       setTimeout(() => {
-        editorRef.current.editor.setContents(initialValue);
+        editorRef.current.editor.setContents(utga);
       }, 100);
     }
-  }, [initialValue]);
-
-  // if (read) {
-  //   return (
-  //     <div
-  //       className="prose max-w-none"
-  //       dangerouslySetInnerHTML={{ __html: utga }}
-  //     />
-  //   );
-  // }
+  }, [utga]);
 
   return (
     <SunEditor
       onChange={handleStringChange}
-      defaultValue={initialValue}
+      defaultValue={utga}
       setContents={utga}
       onLoad={handleEditorLoad}
       setOptions={{
