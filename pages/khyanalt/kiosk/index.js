ec23eaf99361cbf7bbdc4f79a60c1dc7293cf8c8
batkt <sw10d057@gmail.com>
2025-12-14 05:46:22 +0800

    z kiosk butsaav

--- pages/khyanalt/kiosk/index.js
+++ pages/khyanalt/kiosk/index.js
@@ -320,8 +320,9 @@ const Kiosk = () => {
         zakhialgiinDugaar: `${uilchluugchiinId}${ilgeekhDun}`,
         mashiniiDugaar: songogdsonData?.plate_number + " kiosk",
         turul: "kiosk",
-        zogsooliinId: parkingJagsaalt?.[0]?._id,
-        zogsoolUilchluulegchiinId: uilchluugchiinId,
+        // zogsooliinId: parkingJagsaalt?.[0]?._id,
+        // zogsoolUilchluulegchiinId: uilchluugchiinId,
+        // tulukhDun: ilgeekhDun,
       };
       if (parkingJagsaalt?.[0]?.zogsooliinDans) {
         yavuulakhBody["dansniiDugaar"] = parkingJagsaalt?.[0]?.zogsooliinDans;
