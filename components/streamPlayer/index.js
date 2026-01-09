a9bf547a9cbffdcde003dc6705cc91d32d69a4b0
Zola <zzolbaya@gmail.com>
2026-01-09 10:38:23 +0800

    butsaalt2

--- components/streamPlayer/index.js
+++ components/streamPlayer/index.js
@@ -21,7 +21,7 @@ function R2WPlayerComponent({ Camer, USER, PASSWD, nemelteer, PORT, ROOT }) {
 
   useEffect(() => {
     const newPlayer = new R2WPlayer({
-      serverPath: window.location.origin + "/stream",
+      serverPath: "http://127.0.0.1:8083",
       containerId: `videoContainer${Camer}`,
       crossOriginIsolated: true,
       logEnabled: true,
