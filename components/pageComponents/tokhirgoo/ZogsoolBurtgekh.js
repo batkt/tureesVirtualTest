54a9ddfffd80c357cc52d4df282e8ab2f89a30b7
batkt <sw10d057@gmail.com>
2026-02-24 16:43:15 +0800

    тоолохгүй эсэх

--- components/pageComponents/tokhirgoo/ZogsoolBurtgekh.js
+++ components/pageComponents/tokhirgoo/ZogsoolBurtgekh.js
@@ -462,7 +462,7 @@ function ZogsoolBurtgekh(
             </div>
             <div className="col-span-2 border-l-2 border-green-500 pl-4">
               <div className="font-medium dark:text-white">
-                {t("VIP машин тоолох эсэх")}
+                {t("VIP машин тоолохгүй эсэх")}
               </div>
             </div>
             <div className="col-span-2">
