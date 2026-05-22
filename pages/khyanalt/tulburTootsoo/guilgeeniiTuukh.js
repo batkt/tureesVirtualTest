369eb4fb53cd23eadef5148fb5855f9ceeab92d6
buynaaaa24 <abuynaa1211@gmail.com>
2026-05-22 11:42:29 +0800

    d

--- pages/khyanalt/tulburTootsoo/guilgeeniiTuukh.js
+++ pages/khyanalt/tulburTootsoo/guilgeeniiTuukh.js
@@ -112,7 +112,6 @@ const GereeniiUldegdel = React.memo(
       : fallbackUldegdel;
     ugugdul.uldegdel = displayUldegdel;
     ugugdul.tureesiinUldegdel = uldegdelTur;
-    ugugdul.aldangiinUldegdel = uldegdelAld;
     ugugdul.mutate = mutate;
 
     useEffect(() => {
